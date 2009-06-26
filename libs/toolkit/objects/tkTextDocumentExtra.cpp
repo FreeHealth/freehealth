d1188b0a62073b8181a9b8a0c476381c47a248aa
eric.maeker <eric.maeker@gmail.com>
2009-06-26 09:26:22 +0000

    private part to cpp
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@50 571bf770-40aa-11de-9a54-b9394989f659

--- libs/toolkit/objects/tkTextDocumentExtra.cpp
+++ libs/toolkit/objects/tkTextDocumentExtra.cpp
@@ -20,28 +20,114 @@
   \sa tkPrinter
   \ingroup object_toolkit
 */
+
+class tkTextDocumentExtraPrivate {
+public:
+    tkTextDocumentExtraPrivate() : m_Doc(0) {}
+
+    tkPrinter::Presence  m_Presence;
+    tkPrinter::Priority  m_Priority;
+    QString xmlVersion;
+    QString m_Html;
+    mutable bool m_DocCreated;
+    mutable QTextDocument *m_Doc;
+};
+
+tkTextDocumentExtra::tkTextDocumentExtra() : d(0)
+{
+    d = new tkTextDocumentExtraPrivate();
+    d->xmlVersion = TKDOCUMENT_XML_ACTUALVERSION;
+    d->m_Html = "";
+    d->m_Priority = tkPrinter::First;
+    d->m_Presence = tkPrinter::EachPages;
+    d->m_DocCreated = false;
+}
+
+tkTextDocumentExtra::tkTextDocumentExtra( const QString &html, const int presence, const int priority, const QString &version) : d(0)
+{
+    d = new tkTextDocumentExtraPrivate();
+    if (version.isEmpty())
+        d->xmlVersion = TKDOCUMENT_XML_ACTUALVERSION;
+    else
+        d->xmlVersion = version;
+    d->m_Priority = tkPrinter::Priority(priority);
+    d->m_Presence = tkPrinter::Presence(presence);
+    d->m_Html = html;
+    d->m_DocCreated = false;
+}
+
 tkTextDocumentExtra::~tkTextDocumentExtra()
 {
-    if (m_Doc) delete m_Doc;
-    m_Doc = 0;
+    if (d) delete d;
+    d = 0;
 }
+
+void tkTextDocumentExtra::setPriority( tkPrinter::Priority p )
+{ d->m_Priority = p; }
+
+void tkTextDocumentExtra::setPresence( tkPrinter::Presence p )
+{ d->m_Presence = p; }
+
+void tkTextDocumentExtra::setHtml( const QString &html)
+{
+    d->m_Html = html;
+    if (d->m_DocCreated)
+        d->m_Doc->setHtml(html);
+}
+
+tkPrinter::Priority tkTextDocumentExtra::priority() const
+{ return d->m_Priority; }
+
+tkPrinter::Presence tkTextDocumentExtra::presence() const
+{ return d->m_Presence; }
+
+bool tkTextDocumentExtra::lessThan( const tkTextDocumentExtra *h1, const tkTextDocumentExtra *h2 )
+{
+    /** \todo  ? */
+    return true;
+}
+
+QString tkTextDocumentExtra::toXml() const
+{
+    QHash<QString,QString> t;
+    t.insert(TKDOCUMENT_VERSION_XML_TAG, d->xmlVersion);
+    t.insert(TKDOCUMENT_PRESENCE_XML_TAG,QString::number(presence()));
+    t.insert(TKDOCUMENT_PRIORITY_XML_TAG, QString::number(priority()));
+    t.insert(TKDOCUMENT_EXTRA_XML_TAG, "");
+    t.insert(TKDOCUMENT_HTML_XML_TAG, tkGlobal::toHtmlAccent(toHtml()));
+    return tkGlobal::createXml(TKDOCUMENT_GENERAL_XML_TAG,t, 4, false);
+}
+
+tkTextDocumentExtra *tkTextDocumentExtra::fromXml(const QString &xml)
+{
+    if (!xml.contains(QString("<%1>").arg(TKDOCUMENT_GENERAL_XML_TAG)))
+        return new tkTextDocumentExtra();
+    QHash<QString, QString> t;
+    if (!tkGlobal::readXml(xml, TKDOCUMENT_GENERAL_XML_TAG, t, false))
+        return new tkTextDocumentExtra();
+    return new tkTextDocumentExtra(t.value(TKDOCUMENT_HTML_XML_TAG),
+                                   tkPrinter::Presence(t.value(TKDOCUMENT_PRESENCE_XML_TAG).toInt()),
+                                   tkPrinter::Priority(t.value(TKDOCUMENT_PRIORITY_XML_TAG).toInt()),
+                                   t.value(TKDOCUMENT_VERSION_XML_TAG));
+}
+
 
 QString tkTextDocumentExtra::toHtml() const
 {
-    if (m_Doc) {
+    if (d->m_Doc) {
         return document()->toHtml();
     }
-    return m_Html;
+    return d->m_Html;
 }
 
 QTextDocument *tkTextDocumentExtra::document() const
 {
-    if (!m_Doc) {
-        m_Doc = new QTextDocument();
-        m_Doc->setHtml(m_Html);
-        m_DocCreated = true;
+    if (!d->m_Doc) {
+        d->m_Doc = new QTextDocument();
+        d->m_Doc->setHtml(d->m_Html);
+        d->m_DocCreated = true;
     }
-    return m_Doc;
+    return d->m_Doc;
 }
 
 void tkTextDocumentExtra::setTextWidth( qreal width )
--- libs/toolkit/objects/tkTextDocumentExtra.h
+++ libs/toolkit/objects/tkTextDocumentExtra.h
@@ -48,6 +48,8 @@
 #include <QVariant>
 #include <QDebug>
 
+class tkTextDocumentExtraPrivate;
+
 Q_TK_BEGIN_CONSTANTS
 const char* const TKDOCUMENT_GENERAL_XML_TAG   = "tkDocumentExtra";  /*!< \brief tkDocumentExtra Xml Tag. \sa tkDocumentExtra::toXml(), tkDocumentExtra::fromXml() */
 const char* const TKDOCUMENT_VERSION_XML_TAG   = "Version";          /*!< \brief tkDocumentExtra Xml Tag. \sa tkDocumentExtra::toXml(), tkDocumentExtra::fromXml() */
@@ -65,78 +67,28 @@ Q_TK_USING_CONSTANTS
 class Q_TK_EXPORT tkTextDocumentExtra
 {
 public:
-    tkTextDocumentExtra() : m_Doc(0)
-    {
-        xmlVersion = TKDOCUMENT_XML_ACTUALVERSION;
-        m_Html = "";
-        m_Priority = tkPrinter::First;
-        m_Presence = tkPrinter::EachPages;
-        m_DocCreated = false;
-    }
-
-    tkTextDocumentExtra( const QString &html, const int presence = tkPrinter::EachPages, const int priority = tkPrinter::First, const QString &version = QString::null ) : m_Doc(0)
-    {
-        if (version.isEmpty())
-            xmlVersion = TKDOCUMENT_XML_ACTUALVERSION;
-        else
-            xmlVersion = version;
-        m_Priority = tkPrinter::Priority(priority);
-        m_Presence = tkPrinter::Presence(presence);
-        m_Html = html;
-        m_DocCreated = false;
-    }
-
+    tkTextDocumentExtra();
+    tkTextDocumentExtra( const QString &html, const int presence = tkPrinter::EachPages, const int priority = tkPrinter::First, const QString &version = QString::null );
     ~tkTextDocumentExtra();
 
-    void setPriority( tkPrinter::Priority p )  { m_Priority = p; }
-    void setPresence( tkPrinter::Presence p )  { m_Presence = p; }
-    void setHtml( const QString &html)         { m_Html = html; if (m_DocCreated) m_Doc->setHtml(html); }
+    void setPriority( tkPrinter::Priority p );
+    void setPresence( tkPrinter::Presence p );
+    void setHtml( const QString &html);
 
-    tkPrinter::Priority priority() const { return m_Priority; }
-    tkPrinter::Presence presence() const { return m_Presence; }
+    tkPrinter::Priority priority() const;
+    tkPrinter::Presence presence() const;
 
     QTextDocument *document() const;
 
     void setTextWidth( qreal width );
-    bool lessThan( const tkTextDocumentExtra *h1, const tkTextDocumentExtra *h2 )
-    {
-        /** \todo  ? */
-        return true;
-    }
+    bool lessThan( const tkTextDocumentExtra *h1, const tkTextDocumentExtra *h2 );
 
-    QString toXml() const
-    {
-        QHash<QString,QString> t;
-        t.insert(TKDOCUMENT_VERSION_XML_TAG, xmlVersion);
-        t.insert(TKDOCUMENT_PRESENCE_XML_TAG,QString::number(presence()));
-        t.insert(TKDOCUMENT_PRIORITY_XML_TAG, QString::number(priority()));
-        t.insert(TKDOCUMENT_EXTRA_XML_TAG, "");
-        t.insert(TKDOCUMENT_HTML_XML_TAG, tkGlobal::toHtmlAccent(toHtml()));
-        return tkGlobal::createXml(TKDOCUMENT_GENERAL_XML_TAG,t, 4, false);
-    }
-
-    static tkTextDocumentExtra *fromXml(const QString &xml)
-    {
-        if (!xml.contains(QString("<%1>").arg(TKDOCUMENT_GENERAL_XML_TAG)))
-            return new tkTextDocumentExtra();
-        QHash<QString, QString> t;
-        if (!tkGlobal::readXml(xml, TKDOCUMENT_GENERAL_XML_TAG, t, false))
-            return new tkTextDocumentExtra();
-        return new tkTextDocumentExtra(t.value(TKDOCUMENT_HTML_XML_TAG),
-                                       tkPrinter::Presence(t.value(TKDOCUMENT_PRESENCE_XML_TAG).toInt()),
-                                       tkPrinter::Priority(t.value(TKDOCUMENT_PRIORITY_XML_TAG).toInt()),
-                                       t.value(TKDOCUMENT_VERSION_XML_TAG));
-    }
-
+    QString toXml() const;
+    static tkTextDocumentExtra *fromXml(const QString &xml);
     QString toHtml() const;
 
 private:
-    tkPrinter::Presence  m_Presence;
-    tkPrinter::Priority  m_Priority;
-    QString xmlVersion;
-    QString m_Html;
-    mutable bool m_DocCreated;
-    mutable QTextDocument *m_Doc;
+    tkTextDocumentExtraPrivate *d;
 };
 Q_DECLARE_METATYPE(tkTextDocumentExtra)
 
