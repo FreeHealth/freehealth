2ac4c5d03d340cb20b67b2bd2149ad78e467a057
eric.maeker <eric.maeker@gmail.com>
2009-09-08 12:39:09 +0000

    adding help dialog to the coreplugin
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@134 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/coreplugin/dialogs/helpdialog.h
@@ -0,0 +1,82 @@
+/***************************************************************************
+ *   FreeMedicalForms                                                      *
+ *   Copyright (C) 2008-2009 by Eric MAEKER                                *
+ *   eric.maeker@free.fr                                                   *
+ *   All rights reserved.                                                  *
+ *                                                                         *
+ *   This program is a free and open source software.                      *
+ *   It is released under the terms of the new BSD License.                *
+ *                                                                         *
+ *   Redistribution and use in source and binary forms, with or without    *
+ *   modification, are permitted provided that the following conditions    *
+ *   are met:                                                              *
+ *   - Redistributions of source code must retain the above copyright      *
+ *   notice, this list of conditions and the following disclaimer.         *
+ *   - Redistributions in binary form must reproduce the above copyright   *
+ *   notice, this list of conditions and the following disclaimer in the   *
+ *   documentation and/or other materials provided with the distribution.  *
+ *   - Neither the name of the FreeMedForms' organization nor the names of *
+ *   its contributors may be used to endorse or promote products derived   *
+ *   from this software without specific prior written permission.         *
+ *                                                                         *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ *   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ *   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  *
+ *   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  *
+ *   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      *
+ *   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      *
+ *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT    *
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN     *
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+ *   POSSIBILITY OF SUCH DAMAGE.                                           *
+ ***************************************************************************/
+
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef HELPDIALOG_H
+#define HELPDIALOG_H
+
+#include <coreplugin/core_exporter.h>
+
+#include <QDialog>
+
+/**
+ * \file helpdialog.h
+ * \author Eric MAEKER <eric.maeker@free.fr>
+ * \version 0.0.3
+ * \date 08 Sept 2009
+*/
+
+namespace Core {
+namespace Internal {
+class HelpDialogPrivate;
+}
+
+class CORE_EXPORT HelpDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    static void showPage(const QString &page);
+    static void showIndex() { showPage("index.html"); }
+
+protected:
+    explicit HelpDialog( const QString &page = QString::null, QWidget *parent = 0 );
+    void changeEvent(QEvent *event);
+
+private Q_SLOTS:
+    void updateWindowTitle();
+    void fullScreen();
+    void comboActivated(const QString &item);
+
+private:
+    Internal::HelpDialogPrivate *d;
+};
+
+}  // End Core
+
+#endif // HELPDIALOG_H
