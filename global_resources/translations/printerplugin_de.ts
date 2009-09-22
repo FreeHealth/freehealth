b2ad57f1da248616f38e9473ba31e28a351b163a
eric.maeker <eric.maeker@gmail.com>
2009-09-22 10:25:26 +0000

    new translations for printerplugin
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@215 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/printerplugin_de.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Print::Internal::PrinterPreviewerPrivate</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="34"/>
+        <source>preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="47"/>
+        <source>Show page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="54"/>
+        <source>If it&apos;s checked the preview will update at each changes of text.
+ This can be heavy CPU consuming.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="58"/>
+        <source>Automatic update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="65"/>
+        <source>Update preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="75"/>
+        <source>Show Duplicata</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Print::Printer</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="91"/>
+        <source>Each Pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="92"/>
+        <source>First page only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="93"/>
+        <source>Second page only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="94"/>
+        <source>Last page only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="95"/>
+        <source>All pages but first</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="96"/>
+        <source>Odd pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="97"/>
+        <source>Even pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="98"/>
+        <source>Duplicatas only</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Printer</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.cpp" line="777"/>
+        <source>Print Document</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tkPrinter</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.cpp" line="643"/>
+        <source>No content to preview (simpleDraw).</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ global_resources/translations/printerplugin_fr.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Print::Internal::PrinterPreviewerPrivate</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="34"/>
+        <source>preview</source>
+        <translation>prévisualisation</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="47"/>
+        <source>Show page</source>
+        <translation>Voir la page</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="54"/>
+        <source>If it&apos;s checked the preview will update at each changes of text.
+ This can be heavy CPU consuming.</source>
+        <translation>Si la case est cochée alors la prévisualisation se mettra à jour à chaque changement du texte.
+ Ceci peut provoquer une utilisation intensive du processeur.</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="58"/>
+        <source>Automatic update</source>
+        <translation>Mise à jour automatique</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="65"/>
+        <source>Update preview</source>
+        <translation>Mise à jour de la prévisualisation</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printerpreviewer_p.ui" line="75"/>
+        <source>Show Duplicata</source>
+        <translation>Montrer un duplicata</translation>
+    </message>
+</context>
+<context>
+    <name>Print::Printer</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="91"/>
+        <source>Each Pages</source>
+        <translation>Toutes les pages</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="92"/>
+        <source>First page only</source>
+        <translation>Première page seulement</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="93"/>
+        <source>Second page only</source>
+        <translation>Seconde page seulement</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="94"/>
+        <source>Last page only</source>
+        <translation>Dernière page seulement</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="95"/>
+        <source>All pages but first</source>
+        <translation>Toutes les pages sauf la première</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="96"/>
+        <source>Odd pages</source>
+        <translation>Pages impairs</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="97"/>
+        <source>Even pages</source>
+        <translation>Pages pairs</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.h" line="98"/>
+        <source>Duplicatas only</source>
+        <translation>Duplicatas seulement</translation>
+    </message>
+</context>
+<context>
+    <name>Printer</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.cpp" line="777"/>
+        <source>Print Document</source>
+        <translation>Imprimer le document</translation>
+    </message>
+</context>
+<context>
+    <name>tkPrinter</name>
+    <message>
+        <location filename="../../plugins/printerplugin/printer.cpp" line="643"/>
+        <source>No content to preview (simpleDraw).</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
