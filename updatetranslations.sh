44196ff2eaa7c5878d422d917df862d34471b624
eric.maeker <eric.maeker@gmail.com>
2009-09-26 17:35:37 +0000

    update translations script
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@224 571bf770-40aa-11de-9a54-b9394989f659

---
+++ updatetranslations.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+lupdate plugins/aggirplugin/*plugin.pro -no-obsolete
+lupdate plugins/basewidgetsplugin/*plugin.pro -no-obsolete
+lupdate plugins/coreplugin/*plugin.pro -no-obsolete
+lupdate plugins/drugsplugin/*plugin.pro -no-obsolete
+lupdate plugins/fdcoreplugin/*plugin.pro -no-obsolete
+lupdate plugins/fmfcoreplugin/*plugin.pro -no-obsolete
+lupdate plugins/fdmainwindowplugin/*plugin.pro -no-obsolete
+lupdate plugins/listviewplugin/*plugin.pro -no-obsolete
+lupdate plugins/printerplugin/*plugin.pro -no-obsolete
+lupdate plugins/texteditorplugin/*plugin.pro -no-obsolete
+lupdate plugins/usermanagerplugin/*plugin.pro -no-obsolete
+lupdate plugins/xmlioplugin/*plugin.pro -no-obsolete
+
+lupdate libs/utils/*utils.pro -no-obsolete
+lupdate libs/medicalutils/*utils.pro -no-obsolete
+lupdate libs/medintuxutils/*utils.pro -no-obsolete
+lupdate libs/translationutils/*utils.pro -no-obsolete
+
+
