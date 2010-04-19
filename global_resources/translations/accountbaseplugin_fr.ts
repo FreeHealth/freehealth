487e0260a3fe54b59311765594fe0f4296c76803
eric.maeker <eric.maeker@gmail.com>
2010-04-19 10:18:07 +0000

    FreeAccount: - Adding translations files
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@533 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/accountbaseplugin_fr.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Account</name>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/constants.h" line="245"/>
+        <source>Account book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/constants.h" line="246"/>
+        <source>Revenues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/constants.h" line="247"/>
+        <source>Expenses</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AccountDB::AccountBase</name>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/accountbase.cpp" line="486"/>
+        <source>Searching databases into dir %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/accountbase.cpp" line="554"/>
+        <source>Database %1 %2 correctly created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/accountbaseplugin/accountbase.cpp" line="557"/>
+        <source>ERROR : database can not be created %1 %2 %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
