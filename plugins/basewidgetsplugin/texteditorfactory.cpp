608d627ac894f075b38bdb592a9ec90be85a8c75
eric.maeker <eric.maeker@gmail.com>
2010-04-27 12:03:50 +0000

    TextEditorFactory is hold by BaseWidgets
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@560 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/basewidgetsplugin/texteditorfactory.cpp
@@ -0,0 +1,86 @@
+#include "texteditorfactory.h"
+
+//#include <texteditorplugin/texteditor.h>
+
+#include <formmanagerplugin/iformitem.h>
+
+using namespace Editor;
+
+TextEditorFactory::TextEditorFactory(QObject *parent) :
+        IFormWidgetFactory(parent)
+{
+}
+
+TextEditorFactory::~TextEditorFactory()
+{}
+
+bool TextEditorFactory::initialize(const QStringList &arguments, QString *errorString)
+{
+    return true;
+}
+
+bool TextEditorFactory::extensionInitialized()
+{
+    return true;
+}
+
+bool TextEditorFactory::isInitialized() const
+{
+    return true;
+}
+
+QStringList TextEditorFactory::providedWidgets() const
+{
+    return QStringList() << "texteditor" << "richtext" << "richtexteditor" << "editor";
+}
+
+bool TextEditorFactory::isContainer(const int) const
+{
+    return false;
+}
+
+Form::IFormWidget *TextEditorFactory::createWidget(const QString &name, Form::FormItem *linkedObject, QWidget *parent)
+{
+    return new TextEditorForm(linkedObject, parent);
+}
+
+
+TextEditorForm::TextEditorForm(Form::FormItem *linkedObject, QWidget *parent) :
+        Form::IFormWidget(linkedObject,parent), m_Text(0)
+{
+    QHBoxLayout *hb = new QHBoxLayout(this);
+    hb->setMargin(0);
+    hb->setSpacing(0);
+    const QString &options = linkedObject->extraDatas().value("options");
+    TextEditor::Types t = TextEditor::Simple;
+    if (options.compare("FullEditor", Qt::CaseInsensitive) == 0) {
+        t = TextEditor::Full;
+    } else {
+        if (options.compare("WithTable", Qt::CaseInsensitive) == 0) {
+            t |= TextEditor::WithTables;
+        }
+        if (options.compare("WithIO", Qt::CaseInsensitive) == 0) {
+            t |= TextEditor::WithIO;
+        }
+    }
+    m_Text = new TextEditor(this, t);
+    m_Text->setObjectName("TextEditor_" + QString::number(m_LinkedObject->uuid()));
+    hb->addWidget(m_Text);
+    retranslate();
+}
+
+TextEditorForm::~TextEditorForm()
+{
+}
+
+QVariant TextEditorForm::value() const
+{
+    /** \todo this */
+    return "needs to implement script engine and ask it for the scripted value of the form";
+}
+
+void TextEditorForm::retranslate()
+{
+    /** \todo iformitem --> one spec per language ? */
+    //     m_Label->setText( m_LinkedObject->spec()->label() );
+}
