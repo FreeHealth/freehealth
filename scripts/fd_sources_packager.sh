3556633df9db22c3c00fcea525119f9043cdcece
eric.maeker <eric.maeker@gmail.com>
2010-10-05 20:55:37 +0000

    Updating FreeDiams sources packager script (commenting file rights modification)
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@920 571bf770-40aa-11de-9a54-b9394989f659

--- scripts/fd_sources_packager.sh
+++ scripts/fd_sources_packager.sh
@@ -16,83 +16,12 @@ cd $SCRIPT_PATH"/.."
 # rm -R global_resources/doc/fr/*
 # rm -R global_resources/doc/en/*
 
-SOURCE_DIRS="buildspecs \
-doc \
-freediams \
-global_resources/doc \
-global_resources/pixmap \
-libs \
-contrib \
-plugins/coreplugin \
-plugins/drugsbaseplugin \
-plugins/drugsplugin \
-plugins/emptyplugin \
-plugins/fdcoreplugin \
-plugins/fdmainwindowplugin \
-plugins/listviewplugin \
-plugins/printerplugin \
-plugins/saverestoreplugin \
-plugins/templatesplugin \
-plugins/texteditorplugin \
-scripts \
-tests"
 
-SOURCE_FILES="\
-README COPYING INSTALL \
-freediams.pro config.pri checkqtversion.pri \
-updatetranslations.sh \
-global_resources/databases/drugs/drugs*.db \
-global_resources/databases/drugs/iam*.db \
-global_resources/textfiles/freediams.desktop \
-global_resources/translations/*.ts \
-global_resources/translations/qt*.qm \
-plugins/fmf_plugins.pri \
-"
-
-
-FILES_TO_ARCHIVE=""
-
-appendDirFiles() {
-    echo "appendDirFiles "$1
-    for FILE in $1/*
-    do
-        FILES_TO_ARCHIVE+=$FILE" "
-    done
-}
-
-appendDir() {
-    echo "Append Dir "$1
-    appendDirFiles $1
-    if [ -e $1"/*/" ]; then
-        for CHILD in `ls -d $1/*/`;
-        do
-            appendDir $CHILD
-        done;
-    fi;
-}
-
-# Add files
-#for SOURCE in $SOURCE_FILES
-#do
-#   FILES_TO_ARCHIVE+=$SOURCE" "
-#done
-
-# Add dirs
-#for SOURCE in $SOURCE_DIRS
-#do
-#    appendDir $SOURCE
-#done
-
-#echo $FILES_TO_ARCHIVE > text.txt
-
-
-#tar -cvf freediams.tgz \
-#--exclude '.svn' --exclude '.cvsignore' --exclude 'qtc-gdbmacros' \
-#--exclude '_protected' --exclude 'build' --exclude 'bin' --exclude 'packages' --exclude 'rushes' \
-#--exclude 'Makefile*' --exclude '*.pro.user*' \
-#--exclude 'dosages.db' --exclude 'users.db' --exclude '\._.*' \
-#--exclude '*.tgz' --exclude '*.app' --exclude '*.zip' \
-#$FILES_TO_ARCHIVE
+# remove executable flags to files
+#echo "Updating files rights : step 1"
+#sudo find . -type f -exec chmod -R 666 {} \;
+#echo "Updating files rights : step 2"
+#sudo find . -type f -name "*.sh" -exec chmod -R 777 {} \; 
 
 export COPYFILE_DISABLE=true
 
