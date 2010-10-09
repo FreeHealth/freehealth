b23875b6e2a9d466bcdfe9f804a3fd57ebacd0a6
eric.maeker <eric.maeker@gmail.com>
2010-10-09 09:34:52 +0000

    Utils: - added specific code Obj-C code for Mac -> setIconBadge
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@927 571bf770-40aa-11de-9a54-b9394989f659

---
+++ libs/utils/iconbadgealert.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef ICONBADGEALERT_H
+#define ICONBADGEALERT_H
+
+#include <QString>
+
+/**
+ * \file iconbadgealert.h
+ * \author Eric MAEKER <eric.maeker@free.fr>
+ * \version 0.5.0
+ * \date 09 Oct 2010
+*/
+
+namespace Utils {
+namespace MacOs {
+
+    void setIconBadgeLabel(const QString &text);
+
+}  // End namespace MacOs
+}  // End namespace Utils
+
+#endif  // End ICONBADGEALERT_H
---
+++ libs/utils/iconbadgealert.mm
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#include "iconbadgealert.h"
+#include <QString>
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5
+#import <AppKit/NSDockTile.h>
+#import <AppKit/NSApplication.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSAutoreleasePool.h>
+#endif
+
+namespace Utils {
+namespace MacOs {
+
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5
+    void setIconBadgeLabel(const QString &text)
+    {
+        NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+        const char *utf8String = text.toUtf8().constData();
+        NSString *cocoaString = [[NSString alloc] initWithUTF8String:utf8String];
+        [[NSApp dockTile] setBadgeLabel:cocoaString];
+        [cocoaString release];
+        [pool release];
+    }
+
+#else
+
+    void setApplicationLabel(const QString &text) {Q_UNUSED(text);}
+
+#endif
+
+}  // End namespace MacOs
+}  // End namespace Utils
+
