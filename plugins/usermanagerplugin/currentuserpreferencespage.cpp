c68d2b2c70a5813cafa5dc04d994fb0536b8a77e
eric.maeker <eric.maeker@gmail.com>
2010-11-29 00:12:36 +0000

    User: - adding a current user preferences page
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1122 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/usermanagerplugin/currentuserpreferencespage.cpp
@@ -0,0 +1,129 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#include "currentuserpreferencespage.h"
+#include "usermodel.h"
+#include "constants.h"
+#include "widgets/userviewer.h"
+
+#include <utils/log.h>
+#include <utils/global.h>
+#include <translationutils/constanttranslations.h>
+
+#include <coreplugin/icore.h>
+#include <coreplugin/isettings.h>
+
+using namespace UserPlugin;
+using namespace Internal;
+using namespace Trans::ConstantTranslations;
+
+static inline Core::ISettings *settings() { return Core::ICore::instance()->settings(); }
+static inline UserPlugin::UserModel *userModel() {return UserModel::instance();}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////  CurrentUserPreferencesPage  //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+CurrentUserPreferencesPage::CurrentUserPreferencesPage(QObject *parent) :
+        IOptionsPage(parent), m_Widget(0) { setObjectName("CurrentUserPreferencesPage"); }
+
+CurrentUserPreferencesPage::~CurrentUserPreferencesPage()
+{
+    if (m_Widget) delete m_Widget;
+    m_Widget = 0;
+}
+
+QString CurrentUserPreferencesPage::id() const { return objectName(); }
+QString CurrentUserPreferencesPage::name() const { return tkTr(Trans::Constants::USER); }
+QString CurrentUserPreferencesPage::category() const { return tkTr(Trans::Constants::GENERAL); }
+
+void CurrentUserPreferencesPage::resetToDefaults()
+{
+    m_Widget->writeDefaultSettings(settings());
+    m_Widget->setDatasToUi();
+}
+
+void CurrentUserPreferencesPage::applyChanges()
+{
+    if (!m_Widget) {
+        return;
+    }
+    m_Widget->saveToSettings(settings());
+}
+
+void CurrentUserPreferencesPage::finish() { delete m_Widget; }
+
+void CurrentUserPreferencesPage::checkSettingsValidity()
+{
+}
+
+QWidget *CurrentUserPreferencesPage::createPage(QWidget *parent)
+{
+    if (m_Widget)
+        delete m_Widget;
+    m_Widget = new CurrentUserPreferencesWidget(parent);
+    return m_Widget;
+}
+
+
+
+CurrentUserPreferencesWidget::CurrentUserPreferencesWidget(QWidget *parent) :
+        QWidget(parent), m_Viewer(0)
+{
+    setupUi(this);
+    m_Viewer = new UserViewer(this);
+    userLayout->addWidget(m_Viewer);
+    setDatasToUi();
+    /** \todo connect userchange */
+}
+
+void CurrentUserPreferencesWidget::setDatasToUi()
+{
+    if (userModel()->hasCurrentUser())
+        m_Viewer->changeUserTo(userModel()->currentUserIndex().row());
+}
+
+void CurrentUserPreferencesWidget::saveToSettings(Core::ISettings *)
+{
+    if (userModel()->hasCurrentUser()) {
+        label->setFocus();
+        userModel()->submitRow(userModel()->currentUserIndex().row());
+    }
+}
+
+void CurrentUserPreferencesWidget::writeDefaultSettings(Core::ISettings *)
+{
+}
+
+void CurrentUserPreferencesWidget::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ plugins/usermanagerplugin/currentuserpreferencespage.h
@@ -0,0 +1,105 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef CURRENTUSERPREFERENCESPAGE_H
+#define CURRENTUSERPREFERENCESPAGE_H
+
+#include <coreplugin/ioptionspage.h>
+#include "ui_currentuserpreferenceswidget.h"
+
+#include <QPointer>
+#include <QString>
+
+/**
+ * \file currentuserpreferencespage.h
+ * \author Eric MAEKER <eric.maeker@free.fr>
+ * \version 0.1.0
+ * \date 28 Nov 2010
+*/
+
+namespace Core {
+class ISettings;
+}
+
+namespace UserPlugin {
+class UserViewer;
+
+namespace Internal {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////  CurrentUserPreferencesWidget  ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CurrentUserPreferencesWidget : public QWidget, private Ui::CurrentUserPreferencesWidget
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(CurrentUserPreferencesWidget)
+
+public:
+    explicit CurrentUserPreferencesWidget(QWidget *parent = 0);
+    void setDatasToUi();
+
+    static void writeDefaultSettings( Core::ISettings *s );
+
+public Q_SLOTS:
+    void saveToSettings(Core::ISettings *s = 0);
+
+protected:
+    virtual void changeEvent(QEvent *e);
+
+private:
+    UserPlugin::UserViewer *m_Viewer;
+};
+
+}  // End Internal
+
+
+class CurrentUserPreferencesPage : public Core::IOptionsPage
+{
+public:
+    CurrentUserPreferencesPage(QObject *parent = 0);
+    ~CurrentUserPreferencesPage();
+
+    QString id() const;
+    QString name() const;
+    QString category() const;
+
+    void resetToDefaults();
+    void checkSettingsValidity();
+    void applyChanges();
+    void finish();
+
+    QString helpPage() {return QString();}
+
+    static void writeDefaultSettings(Core::ISettings *s) {Internal::CurrentUserPreferencesWidget::writeDefaultSettings(s);}
+
+    QWidget *createPage(QWidget *parent = 0);
+private:
+    QPointer<Internal::CurrentUserPreferencesWidget> m_Widget;
+};
+
+}  // End namespace UserPlugin
+
+#endif // CURRENTUSERPREFERENCESPAGE_H
