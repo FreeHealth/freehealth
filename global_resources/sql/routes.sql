c53ffbff41ed81330e3839071ba890cf845800fc
eric.maeker <eric.maeker@gmail.com>
2010-12-09 14:30:36 +0000

    Drugs: - available routes in SQL
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1219 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/sql/routes.sql
@@ -0,0 +1,142 @@
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (1, "auriculaire","auricular","vorhofflimmern");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (1,'fr','AUR');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (2, "buccale","buccal","mündliche");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (3, "cutanée","dermal","dermal");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (3,'fr','CUT');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (4, "dentaire","dental","spur zahnärztlichen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (5, "endocanalaire","intraductal","intraduktalen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (6, "endocervicale","endocervical","endozervikale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (7, "endosinusale","endosinusale","endosinusale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (8, "endotrachéobronchique","endotracheobronchial","endotracheobronchial");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (9, "gastrique","gastric","magen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (10, "gastro-entérale","gastro-enteral","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (11, "gingivale","gingival","gingivalen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (12, "hémodialyse","hemodialysis","hämodialyse");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (13, "hémofiltration","hemofiltration","hämofiltration");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (14, "implantation","implantation","implantation");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (15, "infiltration","infiltration","infiltration");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (16, "inhalée","inhaled","einatmen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (17, "intestinale","intestinal","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (18, "intra-articulaire","intra-articular","intraartikuläre");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (19, "intra-artérielle","intra-arterial","intraarterielle");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (20, "intra-gingivale","intra-gingival","intra-gingivalen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (21, "intra-murale","intramural","intramuralen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (22, "intra-oculaire","intraocular","intraokularen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (23, "intra-utérine","intrauterine","intrauterine");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (24, "intrabursale","","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (25, "intracardiaque","intracardiac","intrakardialen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (26, "intracaverneuse","intracavernous","intrakavernose");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (27, "intracervicale","intracervical","intrazervikale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (28, "intracisternale","intracisternal","intrazisternale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (29, "intracoronaire","intracoronary","intrakoronare");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (30, "intradermique","intradermal","intradermale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (31, "intradiscale","intradiscal","intradiskale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (32, "intradurale","intradural","intraduralen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (33, "intralymphatique","intralymphatic","intralymphatische");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (34, "intralésionnelle","intralesional","intraläsionale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (35, "intramusculaire","intramuscular","intramuskuläre");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (35,'fr','IM');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (36, "intrapleurale","intrapleural","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (37, "intrapéricardiaque","","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (38, "intrapéritonéale","intraperitoneal","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (39, "intraséreuse","","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (40, "intrathécale","intrathecal","intrathekale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (41, "intratumorale","intratumoral","intratumorale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (42, "intraveineuse","intravenous","intravenös");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (42,'fr','IV');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (43, "intraventriculaire cérébrale","intraventricular brain","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (44, "intravitréenne","intravitreal","intravitreale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (45, "intravésicale","intravesical","intravesikale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (46, "nasale","nasal","nasal");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (47, "ophtalmique","ophtalmic","");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (47,'fr','OPHT');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (48, "orale","oral","");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (48,'fr','PO');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (49, "oropharyngée","oropharyngeal","oropharyngealen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (50, "périarticulaire","periarticular","periartikulären");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (51, "péribulbaire","peribulbar","peribulbärer");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (52, "péridurale","epidural","epidural");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (53, "périneurale","perineural","perineuralen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (54, "périoculaire","periocular","periokuläre");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (55, "rectale","rectal","rektal");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (56, "sous-cutanée","subcutaneous","subkutan");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (56,'fr','SC');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (57, "sublinguale","sublingual","sublinguale");
+-- Abbreviations
+INSERT INTO ROUTE_ABBREV (`ROUTE_ID`,`LANG`,`ABBREV`) VALUES (57,'fr','SubL');
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (58, "transdermique","transdermal","transdermale");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (59, "urétrale","urethral","urethralen");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (60, "vaginale","vaginal","vaginal");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (61, "voie extra corporelle autre","other extra-corporal","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (62, "voie parentérale autre","other parenteral","");
+
+INSERT INTO ROUTES (`ID`,`FR`,`EN`,`DE`) VALUES (63, "épilésionnelle","","");
+
