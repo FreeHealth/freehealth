59b6d4966f260c40f2fb60ca262b4d6cc12df432
eric.maeker <eric.maeker@gmail.com>
2011-01-14 09:48:37 +0000

    FreeToolBox: - ZA db 0.6.0 finalizer
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1421 571bf770-40aa-11de-9a54-b9394989f659

--- global_resources/sql/za_db_finalize.sql
+++ global_resources/sql/za_db_finalize.sql
@@ -1,37 +1,23 @@
 -- /***************************************************************************
--- *   FreeMedicalForms                                                      *
--- *   (C) 2008-2010 by Eric MAEKER, MD                                      *
--- *   eric.maeker@free.fr                                                   *
--- *   All rights reserved.                                                  *
--- *                                                                         *
--- *   This program is a free and open source software.                      *
--- *   It is released under the terms of the new BSD License.                *
--- *                                                                         *
--- *   Redistribution and use in source and binary forms, with or without    *
--- *   modification, are permitted provided that the following conditions    *
--- *   are met:                                                              *
--- *   - Redistributions of source code must retain the above copyright      *
--- *   notice, this list of conditions and the following disclaimer.         *
--- *   - Redistributions in binary form must reproduce the above copyright   *
--- *   notice, this list of conditions and the following disclaimer in the   *
--- *   documentation and/or other materials provided with the distribution.  *
--- *   - Neither the name of the FreeMedForms' organization nor the names of *
--- *   its contributors may be used to endorse or promote products derived   *
--- *   from this software without specific prior written permission.         *
--- *                                                                         *
--- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
--- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
--- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
--- *   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
--- *   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  *
--- *   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  *
--- *   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      *
--- *   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      *
--- *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT    *
--- *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN     *
--- *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
--- *   POSSIBILITY OF SUCH DAMAGE.                                           *
--- ***************************************************************************/
+--  *  The FreeMedForms project is a set of free, open source medical         *
+-- *  applications.                                                          *
+--  *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+--  *  All rights reserved.                                                   *
+--  *                                                                         *
+--  *  This program is free software: you can redistribute it and/or modify   *
+--  *  it under the terms of the GNU General Public License as published by   *
+--  *  the Free Software Foundation, either version 3 of the License, or      *
+--  *  (at your option) any later version.                                    *
+--  *                                                                         *
+--  *  This program is distributed in the hope that it will be useful,        *
+--  *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+--  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+--  *  GNU General Public License for more details.                           *
+--  *                                                                         *
+--  *  You should have received a copy of the GNU General Public License      *
+--  *  along with this program (COPYING.FREEMEDFORMS file).                   *
+--  *  If not, see <http://www.gnu.org/licenses/>.                            *
+--  ***************************************************************************/
 -- /**************************************************************************
 -- *   Main Developper : Eric MAEKER, MD <eric.maeker@free.fr>               *
 -- *   Contributors :                                                        *
@@ -40,8 +26,8 @@
 -- /**
 --  * \file za_db_finalize.sql
 --  * \author Eric MAEKER, MD <eric.maeker@free.fr>
---  * \version 0.4.4
---  * \date 28 July 2010
+--  * \version 0.6.0
+--  * \date 13 Jan 2011
 --  */
 
 -- /**
@@ -49,55 +35,27 @@
 --  *
 --  * \warning SQL commands MUST end with \e ;
 --  *
---  * July 28, 2010 initial release
---  *
---  * NOTE: INSERT INTO "INFORMATIONS" (needs manual updating as to
---  *  i)  the version of the drug db that it is to be known as
---  *  ii) the compatible version of FreeDiams
 --  */
 
 
--- feed table INFORMATIONS (info about the drug data source)
-INSERT INTO "INFORMATIONS" (
-    "VERSION",
-    "NAME",
-    "IDENTIFIANT" ,
-    "COMPAT_VERSION",
-    "PROVIDER",
-    "WEBLINK",
-    "COMPLEMENTARY_WEBSITE",
-    "AUTHOR",
-    "LICENSE",
-    "LICENSE_TERMS",
-    "DATE",
-    "DRUG_UID_NAME",
-    "PACK_MAIN_CODE_NAME",
-    "ATC",
-    "INTERACTIONS",
-    "AUTHOR_COMMENTS",
-    "LANGUAGE_COUNTRY",
-    "DRUGS_NAME_CONSTRUCTOR")
-VALUES (
-    "0.5.2",
-    "xx=South Africa Drug Product Database
-    fr=Base de données thérapeutique d'Afrique du Sud
-    ",
-    "SAEPI_ZA",
-    "0.5.2",
-    "SAEPI : South African Electronic Package Inserts",
-    "http://home.intekom.com/pharm/",
-    "http://home.intekom.com/pharm/",
-    "Eric Maeker, MD",
-    "Public",
-    "Public",
-    CURRENT_DATE,
-    "DRUG_CODE",
-    "UPC",
-    1,
-    1,
-    "",
-    "en_ZA",
-    "NAME"
-);
+UPDATE SOURCES SET
+  LANG            = "fr_FR",
+  WEB             ="http://home.intekom.com/pharm/",
+  COPYRIGHT       ="Public domain",
+  DATE            =CURRENT_DATE,
+  DRUGS_VERSION   ="0.6.0",
+  AUTHORS         ="Eric Maeker, MD (Fr)",
+  VERSION         ="0.6.0",
+  PROVIDER        ="SAEPI : South African Electronic Package Inserts",
+  WEBLINK         ="http://home.intekom.com/pharm/",
+  DRUG_UID_NAME   ="FakeUid",
+  ATC             =1,
+  INTERACTIONS    =1,
+  COMPLEMENTARY_WEBSITE  ="",
+  PACK_MAIN_CODE_NAME    ="FakePUid",
+  DRUGS_NAME_CONSTRUCTOR ="",
+  FREEMEDFORMS_COMPTA_VERSION ="0.6.0",
+  WEBPORTAL_COMPTA_VERSION    ="0.1.0"
+WHERE DATABASE_UID="SAEPI_ZA";
 
-vacuum;
+VACUUM;
