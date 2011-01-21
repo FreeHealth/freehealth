46034a7f8053d26cadca9a0b2d4205696d1f9b58
pm.desombre <pm.desombre@gmail.com>
2011-01-21 12:52:14 +0000

    freeaccount:receipts , insurances
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1507 571bf770-40aa-11de-9a54-b9394989f659

--- plugins/accountbaseplugin/insurancemodel.cpp
+++ plugins/accountbaseplugin/insurancemodel.cpp
@@ -1,78 +1,76 @@
 /***************************************************************************
- *   FreeMedicalForms                                                      *
- *   (C) 2008-2010 by Eric MAEKER, MD                                     **
+ *   Copyright (C) 2009 by Docteur Pierre-Marie Desombre, GP               *
+ *   pm.desombre@medsyn.fr                                                 *
+ *   original from Eric Maeker, MD.                                        *
  *   eric.maeker@free.fr                                                   *
- *   All rights reserved.                                                  *
  *                                                                         *
- *   This program is a free and open source software.                      *
- *   It is released under the terms of the new BSD License.                *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
- *   Redistribution and use in source and binary forms, with or without    *
- *   modification, are permitted provided that the following conditions    *
- *   are met:                                                              *
- *   - Redistributions of source code must retain the above copyright      *
- *   notice, this list of conditions and the following disclaimer.         *
- *   - Redistributions in binary form must reproduce the above copyright   *
- *   notice, this list of conditions and the following disclaimer in the   *
- *   documentation and/or other materials provided with the distribution.  *
- *   - Neither the name of the FreeMedForms' organization nor the names of *
- *   its contributors may be used to endorse or promote products derived   *
- *   from this software without specific prior written permission.         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
  *                                                                         *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
- *   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
- *   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  *
- *   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  *
- *   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      *
- *   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      *
- *   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT    *
- *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN     *
- *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
- *   POSSIBILITY OF SUCH DAMAGE.                                           *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       Pierre-Marie DESOMBRE <pm.desombre@medsyn.fr>                     *
+ *       NAME <MAIL@ADRESS>                                                *
  ***************************************************************************/
 #include "insurancemodel.h"
-#include "accountbase.h"
 #include "constants.h"
+#include "accountbase.h"
 
-#include <utils/log.h>
+#include <translationutils/constanttranslations.h>
 
+#include <QAbstractTableModel>
 #include <QSqlTableModel>
+
+#include <QDebug>
+
+
 
 using namespace AccountDB;
 
+using namespace Trans::ConstantTranslations;
+
+
 namespace AccountDB {
 namespace Internal {
-
 class InsuranceModelPrivate
 {
 public:
-    InsuranceModelPrivate(InsuranceModel *parent) : m_SqlTable(0), m_IsDirty(false), q(parent)
+    InsuranceModelPrivate(InsuranceModel *parent) : m_SqlTable(0), q(parent)
     {
-        m_SqlTable = new QSqlTableModel(q, QSqlDatabase::database(Constants::DB_ACCOUNTANCY));
-        m_SqlTable->setTable(AccountDB::AccountBase::instance()->table(Constants::Table_Insurance));
-//        m_SqlTable->setFilter(USER_UID);
+        m_SqlTable = new QSqlTableModel(q,QSqlDatabase::database(Constants::DB_ACCOUNTANCY));
+        m_SqlTable->setTable(AccountBase::instance()->table(Constants::Table_Insurance));
     }
+
     ~InsuranceModelPrivate () {}
 
 public:
     QSqlTableModel *m_SqlTable;
-    bool m_IsDirty;
+
 
 private:
     InsuranceModel *q;
 };
-
-}  // End namespace Internal
-}  // End namespace AccountDB
+}
+}
 
 
 
 InsuranceModel::InsuranceModel(QObject *parent) :
         QAbstractTableModel(parent), d(new Internal::InsuranceModelPrivate(this))
 {
-//    d->m_SqlTable->setEditStrategy(QSqlTableModel::OnManualSubmit);
     d->m_SqlTable->setEditStrategy(QSqlTableModel::OnFieldChange);
     d->m_SqlTable->select();
 }
@@ -88,7 +86,11 @@ InsuranceModel::~InsuranceModel()
 
 int InsuranceModel::rowCount(const QModelIndex &parent) const
 {
-    return d->m_SqlTable->rowCount(parent);
+    int rows = 0;
+    d->m_SqlTable->setFilter("");
+    d->m_SqlTable->select();
+    rows = d->m_SqlTable->rowCount(parent);
+    return rows;
 }
 
 int InsuranceModel::columnCount(const QModelIndex &parent) const
@@ -96,23 +98,27 @@ int InsuranceModel::columnCount(const QModelIndex &parent) const
     return d->m_SqlTable->columnCount(parent);
 }
 
-void InsuranceModel::setUserUuid(const QString &uuid)
-{
-    QHash<int, QString> where;
-    where.insert(Constants::BANKDETAILS_USER_UID, QString("='%1'").arg(uuid));
-    d->m_SqlTable->setFilter(AccountBase::instance()->getWhereClause(Constants::Table_Insurance, where));
-}
 
 QVariant InsuranceModel::data(const QModelIndex &index, int role) const
 {
-    return d->m_SqlTable->data(index, role);
+    QVariant data ;
+    if(!index.isValid()){
+       qWarning() << __FILE__ << QString::number(__LINE__) << "index is not valid" ;
+       data = QVariant();
+       }
+    if(role == Qt::DisplayRole){
+        data = d->m_SqlTable->data(index, Qt::DisplayRole);
+        }	
+    if (role == Qt::EditRole)
+    {
+    	data = d->m_SqlTable->data(index, Qt::EditRole);
+        }
+    return data;
 }
 
 bool InsuranceModel::setData(const QModelIndex &index, const QVariant &value, int role)
 {
-    bool ret = d->m_SqlTable->setData(index, value, role);
-    d->m_IsDirty = d->m_SqlTable->isDirty(index);
-    return ret;
+    return d->m_SqlTable->setData(index, value, role);
 }
 
 
@@ -123,35 +129,39 @@ QVariant InsuranceModel::headerData(int section, Qt::Orientation orientation, in
 
 bool InsuranceModel::insertRows(int row, int count, const QModelIndex &parent)
 {
-    d->m_IsDirty = true;
-//    d->m_SqlTable->setEditStrategy(QSqlTableModel::OnRowChange);
-    bool ret = d->m_SqlTable->insertRows(row, count, parent);
-//    d->m_SqlTable->setEditStrategy(QSqlTableModel::OnManualSubmit);
-    return ret;
+      bool ret = d->m_SqlTable->insertRows(row, count, parent);
+      return ret;
 }
 
 bool InsuranceModel::removeRows(int row, int count, const QModelIndex &parent)
 {
-    d->m_IsDirty = true;
     return d->m_SqlTable->removeRows(row, count, parent);
+}
+
+bool InsuranceModel::isDirty() const
+{
+    return true;
 }
 
 bool InsuranceModel::submit()
 {
     if (d->m_SqlTable->submitAll()) {
-        d->m_IsDirty = false;
         return true;
     }
     return false;
 }
 
-void InsuranceModel::revert()
+void  InsuranceModel::revert()
 {
-    d->m_IsDirty = false;
     d->m_SqlTable->revert();
 }
 
-bool InsuranceModel::isDirty() const
-{
-    return d->m_IsDirty;
+QSqlError InsuranceModel::lastError(){
+    return d->m_SqlTable->lastError();
 }
+
+void InsuranceModel::setFilter(const QString & filter){
+    d->m_SqlTable->setFilter(filter);
+    d->m_SqlTable->select();
+}
+
