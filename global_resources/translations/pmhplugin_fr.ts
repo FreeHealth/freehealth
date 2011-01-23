1ef056035b60b86826f2877d62ac2d6be306b20f
eric.maeker <eric.maeker@gmail.com>
2011-01-23 15:45:18 +0000

    FreeMedForms: - New translations
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1523 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/pmhplugin_fr.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>PMH::Internal::PmhBase</name>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="145"/>
+        <source>Unable to connect the Templates host.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="145"/>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="151"/>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="155"/>
+        <source>Please contact dev team.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="151"/>
+        <source>Unable to create the Templates database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhbase.cpp" line="155"/>
+        <source>Unable to connect the Templates database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PMH::Internal::PmhMode</name>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhmode.cpp" line="28"/>
+        <source>Medical History</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhmode.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PMH::Internal::PmhViewer</name>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="32"/>
+        <source>Past Medical History</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="61"/>
+        <source>General information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="67"/>
+        <source>Personal label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="77"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="87"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="110"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="120"/>
+        <source>ICD10 Coding</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="125"/>
+        <source>Management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="130"/>
+        <source>Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/pmhplugin/pmhviewer.ui" line="135"/>
+        <source>Links</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
