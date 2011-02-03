cf24947f16378602627c91788237b186770b7290
eric.maeker <eric.maeker@gmail.com>
2011-02-03 22:09:52 +0000

    Views: - new Extended TableView
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1576 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/listviewplugin/tableview.cpp
@@ -0,0 +1,158 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+
+#include "tableview.h"
+#include "constants.h"
+#include "viewmanager.h"
+
+#include <coreplugin/actionmanager/actionmanager.h>
+#include <coreplugin/contextmanager/contextmanager.h>
+#include <coreplugin/icore.h>
+#include <coreplugin/uniqueidmanager.h>
+
+
+using namespace Views;
+using namespace Internal;
+
+static inline Core::ContextManager *contextManager() { return Core::ICore::instance()->contextManager(); }
+
+
+namespace Views {
+namespace Internal {
+
+class TableViewPrivate
+{
+public:
+    TableViewPrivate(QWidget *parent, Constants::AvailableActions actions) :
+            m_Parent(parent),
+            m_Actions(actions),
+            m_Context(0),
+            m_ExtView(0)
+    {
+    }
+
+    ~TableViewPrivate()
+    {
+        if (m_ExtView)
+            delete m_ExtView;
+        m_ExtView = 0;
+    }
+
+    void calculateContext()
+    {
+        m_Context->clearContext();
+        if (m_Actions & Constants::MoveUpDown)
+            m_Context->addContext(Core::ICore::instance()->uniqueIDManager()->uniqueIdentifier(Constants::C_BASIC_MOVE));
+
+        if (m_Actions & Constants::AddRemove)
+            m_Context->addContext(Core::ICore::instance()->uniqueIDManager()->uniqueIdentifier(Constants::C_BASIC_ADDREMOVE));
+    }
+
+public:
+    QWidget *m_Parent;
+    Constants::AvailableActions m_Actions;
+    ViewContext *m_Context;
+    QToolBar *m_ToolBar;
+    QString m_ContextName;
+    ExtendedView *m_ExtView;
+};
+
+}  // End Internal
+}  // End Views
+
+
+/** \brief Constructor */
+TableView::TableView(QWidget *parent, Constants::AvailableActions actions) :
+        QTableView(parent),
+        d(0)
+{
+    static int handler = 0;
+    ++handler;
+    QObject::setObjectName("TableView_"+QString::number(handler));
+    setProperty(Constants::HIDDEN_ID, "xx");
+    d = new Internal::TableViewPrivate(this, actions);
+
+    // Create the Manager instance and context
+    ViewManager::instance();
+    d->m_Context = new ViewContext(this);
+    d->calculateContext();
+    contextManager()->addContextObject(d->m_Context);
+
+    // Create the extension of the view
+    d->m_ExtView = new ExtendedView(this);
+}
+
+TableView::~TableView()
+{
+    contextManager()->removeContextObject(d->m_Context);
+}
+
+void TableView::setActions(Constants::AvailableActions actions)
+{
+    d->m_Actions = actions;
+    d->calculateContext();
+}
+
+void TableView::hideButtons() const
+{
+    d->m_ExtView->hideButtons();
+}
+
+void TableView::showButtons()
+{
+    d->m_ExtView->showButtons();
+}
+
+void TableView::useContextMenu(bool state)
+{
+    d->m_ExtView->useContextMenu(state);
+}
+
+void TableView::addItem()
+{
+    qWarning() << "TableView::addItem()";
+    setFocus();
+    d->m_ExtView->addItem();
+}
+
+void TableView::removeItem()
+{
+    setFocus();
+    d->m_ExtView->removeItem();
+}
+
+void TableView::moveDown()
+{
+    setFocus();
+    d->m_ExtView->moveDown();
+}
+
+void TableView::moveUp()
+{
+    setFocus();
+    d->m_ExtView->moveUp();
+}
+
---
+++ plugins/listviewplugin/tableview.h
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2010 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+
+#ifndef TABLEVIEW_H
+#define TABLEVIEW_H
+
+
+#include <listviewplugin/listview_exporter.h>
+#include <listviewplugin/extendedview.h>
+#include <listviewplugin/constants.h>
+
+#include <QWidget>
+#include <QTableView>
+#include <QAbstractItemModel>
+#include <QAbstractItemView>
+#include <QFlags>
+#include <QMenu>
+
+/**
+ * \file tableview.h
+ * \author Eric MAEKER <eric.maeker@free.fr>
+ * \version 0.5.0
+ * \date 03 Feb 2011
+*/
+
+
+namespace Views {
+namespace Internal {
+class TableViewPrivate;
+class ExtendedViewPrivate;
+}
+
+class LISTVIEW_EXPORT TableView : public QTableView
+{
+    Q_OBJECT
+    friend class TableViewPrivate;
+public:
+    TableView(QWidget *parent = 0, Constants::AvailableActions actions = Constants::DefaultActions);
+    virtual ~TableView();
+
+    void setActions(Constants::AvailableActions actions);
+
+    void hideButtons() const;
+    void showButtons();
+    void useContextMenu(bool state = true);
+
+public Q_SLOTS:
+    virtual void addItem();
+    virtual void removeItem();
+    virtual void moveDown();
+    virtual void moveUp();
+
+Q_SIGNALS:
+    void addRequested();
+    void removeRequested();
+    void moveUpRequested();
+    void moveDownRequested();
+
+private:
+    Internal::TableViewPrivate *d;
+};
+
+}  // End Views
+
+
+#endif // TABLEVIEW_H
