6f0d40373f0782503d3fd32d72e2a9e9d0bf7048
eric.maeker <eric.maeker@gmail.com>
2011-03-02 08:20:11 +0000

    FreeAccount: - reformatting AssetIO as an example of coding
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@1727 571bf770-40aa-11de-9a54-b9394989f659

--- plugins/accountmanagerplugin/assets/assetsIO.cpp
+++ plugins/accountmanagerplugin/assets/assetsIO.cpp
@@ -1,4 +1,7 @@
 #include "assetsIO.h"
+#include "assetmodel.h
+
+#include <accountbaseplugin/constants.h>
 
 #include <QDebug>
 #include <QMessageBox>
@@ -7,31 +10,33 @@
 using namespace AccountDB;
 using namespace Constants;
 
-AssetsIO::AssetsIO(QObject * parent){
-    m_assetModel = new AssetModel(this);
+AssetsIO::AssetsIO(QObject *parent) :
+    QObject(parent), m_assetModel(new AssetModel(this))
+{
 }
 
-AssetsIO::~AssetsIO(){}
+AssetsIO::~AssetsIO()
+{
+}
 
-bool AssetsIO::insertIntoAssets(QHash<int,QVariant> & hashValues){
+bool AssetsIO::insertIntoAssets(QHash<int,QVariant> &hashValues)
+{
     bool ret = true;
     int rowBefore = m_assetModel->rowCount(QModelIndex());
     qDebug() << __FILE__ << QString::number(__LINE__) << " rowBefore = " << QString::number(rowBefore);
-    if (m_assetModel->insertRows(rowBefore,1,QModelIndex()))
-    {
-    	  qWarning() << __FILE__ << QString::number(__LINE__) << "Row inserted !" ;
-        }
+    if (m_assetModel->insertRows(rowBefore,1,QModelIndex())) {
+        qWarning() << __FILE__ << QString::number(__LINE__) << "Row inserted !" ;
+    }
     QVariant data;
     for(int i = 1 ; i < ASSETS_MaxParam ; i ++){
-         data = hashValues.value(i);
-         qDebug() << __FILE__ << QString::number(__LINE__) << " data + i =" << data.toString()+" "+QString::number(i);
-         if (!m_assetModel-> setData(m_assetModel->index(rowBefore,i), data ,Qt::EditRole))
-            {
-            	qWarning() << __FILE__ << QString::number(__LINE__) << " asset model error = " 
-                                                                    << m_assetModel->lastError().text() ;
-                }
+        data = hashValues.value(i);
+        qDebug() << __FILE__ << QString::number(__LINE__) << " data + i =" << data.toString()+" "+QString::number(i);
+        if (!m_assetModel-> setData(m_assetModel->index(rowBefore,i), data ,Qt::EditRole)) {
+            qWarning() << __FILE__ << QString::number(__LINE__) << " asset model error = "
+                    << m_assetModel->lastError().text() ;
         }
-        m_assetModel->submit();
+    }
+    m_assetModel->submit();
     if (m_assetModel->rowCount(QModelIndex()) == rowBefore) {
         QMessageBox::warning(0,trUtf8("Warning AssetsIO : "),trUtf8("Error = ") 
                              + m_assetModel->lastError().text(),
@@ -41,11 +46,11 @@ bool AssetsIO::insertIntoAssets(QHash<int,QVariant> & hashValues){
     return ret;
 }
 
-bool AssetsIO::deleteAsset(int row){
+bool AssetsIO::deleteAsset(int row)
+{
     bool b = true;
-    if (!m_assetModel->removeRow(row,QModelIndex()))
-    {
-    	  b = false;
-        }
+    if (!m_assetModel->removeRow(row,QModelIndex())) {
+        b = false;
+    }
     return b;
 }
--- plugins/accountmanagerplugin/assets/assetsIO.h
+++ plugins/accountmanagerplugin/assets/assetsIO.h
@@ -1,19 +1,35 @@
 #ifndef ASSETSIO_H
 #define ASSETSIO_H
-#include "../../accountbaseplugin/assetmodel.h"
-#include "../../accountbaseplugin/constants.h"//<accountbaseplugin/constants.h>
-using namespace AccountDB;
-class AssetsIO : public QObject {
+
+#include <QObject>
+#include <QVariant>
+#include <QHash>
+
+// #include "../../accountbaseplugin/constants.h"//<accountbaseplugin/constants.h>  A eviter+++
+
+// #include "../../accountbaseplugin/assetmodel.h"
+// NON à la place déclare la class comme ça et met le include dans le cpp
+// Intérêt == si modification du assetModel header pas de recompilation de cet objet avec ma méthode
+// alors qu'il aurait été recompilé avec ton include.
+
+namespace AccountDB {
+class AssetModel;
+}
+
+class AssetsIO : public QObject
+{
     Q_OBJECT
-    public :
-        AssetsIO(QObject * parent);
-        ~AssetsIO();
-        AssetModel * getModelAssets();
-        bool insertIntoAssets(QHash<int,QVariant> & hashValues);
-        bool deleteAsset(int row);
-    private :
-        AssetModel * m_assetModel;
+public:
+    AssetsIO(QObject *parent);
+    ~AssetsIO();
+
+    AssetModel *getModelAssets();
+    bool insertIntoAssets(QHash<int,QVariant> &hashValues);
+    bool deleteAsset(int row);
+
+private:
+    AssetModel *m_assetModel;  // ça compilera sans problème
 };
 
-#endif
 
+#endif
