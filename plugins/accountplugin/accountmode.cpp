dda7894f1c834623f2e405dec0ebe9bfa479b5c8
eric.maeker <eric.maeker@gmail.com>
2011-04-13 22:28:32 +0000

    Account: - creating account mode - adding some FreeMedForms specific code - using LOG macros
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2042 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/accountplugin/accountmode.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2011 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#include "accountmode.h"
+#include "accountview.h"
+
+#include <coreplugin/icore.h>
+#include <coreplugin/itheme.h>
+#include <coreplugin/constants_menus.h>
+#include <coreplugin/constants_icons.h>
+
+using namespace Account::Internal;
+
+static inline Core::ITheme *theme()  { return Core::ICore::instance()->theme(); }
+
+AccountMode::AccountMode(QObject *parent) :
+    Core::BaseMode(parent)
+{
+    setName(tr("Accountancy"));
+    setIcon(theme()->icon(Core::Constants::ICONACCOUNTMODE, Core::ITheme::BigIcon));
+    setPriority(Core::Constants::P_MODE_ACCOUNT);
+    setUniqueModeName(Core::Constants::MODE_ACCOUNT);
+//    const QList<int> &context;
+//    setContext();
+    setWidget(new Account::AccountView);
+}
---
+++ plugins/accountplugin/accountmode.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2011 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef ACCOUNTMODE_H
+#define ACCOUNTMODE_H
+
+#include <coreplugin/modemanager/basemode.h>
+
+namespace Account {
+namespace Internal {
+
+class AccountMode : public Core::BaseMode
+{
+    Q_OBJECT
+public:
+    explicit AccountMode(QObject *parent = 0);
+
+private:
+
+};
+
+}  // End namespace Internal
+}  // End namespace Account
+
+#endif // ACCOUNTMODE_H
