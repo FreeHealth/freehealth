2199e9beb7ec487a5911226b517e996c421cb002
eric.maeker <eric.maeker@gmail.com>
2011-05-04 17:38:30 +0000

    Scripts: - mac release can compil from the source package
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2159 571bf770-40aa-11de-9a54-b9394989f659

--- scripts/mac_release.sh
+++ scripts/mac_release.sh
@@ -43,20 +43,158 @@ showHelp()
   echo "Usage : $SCRIPT_NAME <options>"
   echo "Options :"
   echo "          -b  Bundle name"
+  echo "          -s  Build from source package (you need to create the source package)"
   echo "          -h  show this help"
   echo "Win32 port can be build under Linux using crosscompilation"
   exit 0
 }
 
+buildFromSourcePackage()
+{
+  SOURCE_PACKAGE=$SOURCES_PATH$PROJECT"fullsources-"$VERSION".tgz"
+  # test source package
+  if [ ! -e $SOURCE_PACKAGE ] ; then
+      echo $SOURCE_PACKAGE" not found"
+      exit 123;
+  fi
+  SOURCES_PATH=$SCRIPT_PATH$PROJECT"-"$VERSION"/"
+#  if [ -e $SOURCES_PATH ] ; then
+#    rm -Rf $SOURCES_PATH
+#  fi
+  TO=$SCRIPT_PATH
+  tar xzf $SOURCE_PACKAGE -C $TO
+  PACKAGES_PATH=$SOURCES_PATH"/packages/"
+  PROJECT_FILE=$SOURCES_PATH$PROJECT.pro;
+}
 
-while getopts "b:h" option
+buildApp()
+{
+  #####################################
+  # 1. make clean                     #
+  #####################################
+  echo "*** Cleaning build path..."
+  cd $SOURCES_PATH
+  rm -R ./bin/$PROJECT/plugins
+
+  #rm -R build
+  #rm -R bin
+  #$QMAKE_BIN && make clean
+
+
+  #####################################
+  # 2. Build process                  #
+  #####################################
+  # build app
+  echo "*** Building "$BUNDLE_NAME" "$VERSION
+  cd $SOURCES_PATH
+  QMAKE_SPEC="-r -spec macx-g++ CONFIG+=release CONFIG-=debug_and_release LOWERED_APPNAME=$PROJECT"
+
+   MAKE_STEP=`$QMAKE_BIN $PROJECT_FILE $QMAKE_SPEC`
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+     echo "   *** Error: qmake step wrong ***"
+     exit 123
+   else
+     echo "   *** QMake done"
+   fi
+
+   MAKE_STEP=`make`
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+     echo "   *** Error: make step wrong ***"
+     exit 123
+   else
+     echo "   *** Make done"
+   fi
+
+   MAKE_STEP=`make install`
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+     echo "   *** Error: install step wrong ***"
+     exit 123
+   else
+     echo "   *** Make Install done"
+   fi
+}
+
+linkQtLibs()
+{
+   # link frameworks
+   echo "*** Linking Frameworks..."
+   cd $PACKAGES_PATH/mac/$BUNDLE_NAME
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+     echo "*** Error: Installation not found ***"
+     exit 123
+   else
+     echo "moving to cd $PACKAGES_PATH/mac/$BUNDLE_NAME"
+   fi
+
+   MAKE_STEP=`$MAC_SCRIPTS_PATH/macDeploy.sh -a $BUNDLE_NAME -p imageformats -p sqldrivers -p accessible`
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+   echo "*** Error: Deployement step wrong ***"
+   exit 123
+   fi
+}
+
+linkMySqlLib()
+{
+   # Deploy MySql lib
+   ACTUAL_PATH=`pwd`
+   cd $PACKAGES_PATH/mac/$BUNDLE_NAME/$BUNDLE_NAME.app
+   MYSQL_PLUGIN="./Contents/plugins/qt/sqldrivers/libqsqlmysql.dylib"
+   FRAMEWORK_PATH="./Contents/Frameworks"
+   mysqllib=""
+   for n in `otool -LX "$MYSQL_PLUGIN" | grep mysqlclient` ; do
+     path=`echo $n | grep mysqlclient`
+     if [ $path ] ; then
+       mysqllib=$path
+       echo $path
+     fi
+     done
+   #    if [ -e $mysqllib ] ; then
+   #        echo "No MySql client to install"
+   #    else
+   echo "Linking MySql client lib from $mysqllib to $FRAMEWORK_PATH for $BUNDLE_NAME"
+   mysqllib_source=`basename $mysqllib`
+   mysqllib_source=`locate $mysqllib_source`
+   cp $mysqllib_source $FRAMEWORK_PATH
+   name=`basename $mysqllib`
+   install_name_tool -change $mysqllib @executable_path/../Frameworks/$name "$MYSQL_PLUGIN"
+   #    fi
+   cd $ACTUAL_PATH
+}
+
+createDmg()
+{
+   # clean old dmg and create new one
+   echo "*** Creating DMG archive..."
+   rm *.dmg
+   MAKE_STEP=`$MAC_SCRIPTS_PATH/release_dmg.sh -a $BUNDLE_NAME -p ./ -s 150 -f $MAC_SCRIPTS_PATH/../`
+   MAKE_STEP=$?
+   if [ ! $MAKE_STEP = 0 ]; then
+     echo "*** Error: DMG creation step wrong ***"
+     exit 123
+   fi
+
+   # move archive to right place
+   mv $BUNDLE_NAME.dmg $PACKAGES_PATH/$BUNDLE_NAME-$VERSION.dmg
+ }
+
+while getopts "b:sh" option
 do
 echo "(-- option:$option  $OPTIND - '$OPTARG' --)"
         case $option in
-                b) BUNDLE_NAME=$OPTARG; PROJECT=`echo $OPTARG | tr '[A-Z]' '[a-z]'`; PROJECT_FILE=$SOURCES_PATH$PROJECT.pro
+                b) BUNDLE_NAME=$OPTARG;
+                   PROJECT=`echo $OPTARG | tr '[A-Z]' '[a-z]'`;
+                   PROJECT_FILE=$SOURCES_PATH$PROJECT.pro;
+                   # get version number of FreeDiams from the project file
+                   VERSION=`cat $SOURCES_PATH$PROJECT/$PROJECT.pro | grep "PACKAGE_VERSION" -m 1 | cut -d = -s -f2 | tr -d ' '`
                 ;;
 		h) showHelp
 		;;
+                s) buildFromSourcePackage
         esac
 done
 
@@ -69,9 +207,6 @@ if [ -z "$BUNDLE_NAME" ] ; then
     exit 123
 fi
 
-# get version number of FreeDiams from the project file
-VERSION=`cat $SOURCES_PATH$PROJECT/$PROJECT.pro | grep "PACKAGE_VERSION" -m 1 | cut -d = -s -f2 | tr -d ' '`
-
 if [ -z $VERSION ] ; then
    echo "No version number found"
    exit
@@ -82,109 +217,13 @@ if [ ! -e $PACKAGES_PATH ] ; then
 fi
 
 echo "*** Creating package for $BUNDLE_NAME $VERSION"
+echo "      * project file $PROJECT_FILE"
+echo "      * from source path $SOURCES_PATH"
+echo "      * to package path $PACKAGES_PATH"
 
+buildApp
+linkQtLibs
+linkMySqlLib
+createDmg
 
-#####################################
-# 1. make clean                     #
-#####################################
-echo "*** Cleaning build path..."
-cd $SOURCES_PATH
-rm -R ./bin/$PROJECT/plugins
-
-#rm -R build
-#rm -R bin
-#$QMAKE_BIN && make clean
-
-
-#####################################
-# 2. Build process                  #
-#####################################
-   # build app
-   echo "*** Building "$BUNDLE_NAME" "$VERSION
-   cd $SOURCES_PATH
-   QMAKE_SPEC="-r -spec macx-g++ CONFIG+=release CONFIG-=debug_and_release LOWERED_APPNAME=$PROJECT"
-
-   MAKE_STEP=`$QMAKE_BIN $PROJECT_FILE $QMAKE_SPEC`
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "   *** Error: qmake step wrong ***"
-      exit 123
-   else
-      echo "   *** QMake done"
-   fi
-
-   MAKE_STEP=`make`
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "   *** Error: make step wrong ***"
-      exit 123
-   else
-      echo "   *** Make done"
-   fi
-
-   MAKE_STEP=`make install`
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "   *** Error: install step wrong ***"
-      exit 123
-   else
-      echo "   *** Make Install done"
-   fi
-
-
-   # link frameworks
-   echo "*** Linking Frameworks..."
-   cd $PACKAGES_PATH/mac/$BUNDLE_NAME
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "*** Error: Installation not found ***"
-      exit 123
-   else
-      echo "moving to cd $PACKAGES_PATH/mac/$BUNDLE_NAME"
-   fi
-
-   MAKE_STEP=`$MAC_SCRIPTS_PATH/macDeploy.sh -a $BUNDLE_NAME -p imageformats -p sqldrivers -p accessible`
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "*** Error: Deployement step wrong ***"
-      exit 123
-   fi
-
-    # Deploy MySql lib
-    ACTUAL_PATH=`pwd`
-    cd $PACKAGES_PATH/mac/$BUNDLE_NAME/$BUNDLE_NAME.app
-    MYSQL_PLUGIN="./Contents/plugins/qt/sqldrivers/libqsqlmysql.dylib"
-    FRAMEWORK_PATH="./Contents/Frameworks"
-    mysqllib=""
-    for n in `otool -LX "$MYSQL_PLUGIN" | grep mysqlclient` ; do
-        path=`echo $n | grep mysqlclient`
-        if [ $path ] ; then
-            mysqllib=$path
-            echo $path
-        fi
-    done
-#    if [ -e $mysqllib ] ; then
-#        echo "No MySql client to install"
-#    else
-        echo "Linking MySql client lib from $mysqllib to $FRAMEWORK_PATH for $BUNDLE_NAME"
-        cp $mysqllib $FRAMEWORK_PATH
-        name=`basename $mysqllib`
-        install_name_tool -change $mysqllib @executable_path/../Frameworks/$name "$MYSQL_PLUGIN"
-#    fi
-    cd $ACTUAL_PATH
-
-   # clean old dmg and create new one
-   echo "*** Creating DMG archive..."
-   rm *.dmg
-   MAKE_STEP=`$MAC_SCRIPTS_PATH/release_dmg.sh -a $BUNDLE_NAME -p ./ -s 150 -f $MAC_SCRIPTS_PATH/../`
-   MAKE_STEP=$?
-   if [ ! $MAKE_STEP = 0 ]; then
-      echo "*** Error: DMG creation step wrong ***"
-      exit 123
-   fi
-
-   # move archive to right place
-   mv $BUNDLE_NAME.dmg $PACKAGES_PATH/$BUNDLE_NAME-$VERSION.dmg
-
-   # nothing to do next so exit
-   exit 0
+exit 0
