8d4f2ec1d8e563d76e0c3cb29eecb0d4b12bf659
eric.maeker <eric.maeker@gmail.com>
2011-05-12 12:10:42 +0000

    Core: - adding a new IClass -> for the first run dialog
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2171 571bf770-40aa-11de-9a54-b9394989f659

---
+++ plugins/coreplugin/ifirstconfigurationpage.h
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2011 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developper : Eric MAEKER, <eric.maeker@free.fr>                  *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef IFIRSTCONFIGURATIONPAGE_H
+#define IFIRSTCONFIGURATIONPAGE_H
+
+#include <coreplugin/core_exporter.h>
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtGui/QWizardPage>
+
+/**
+ * \file ifirstconfigurationpage.h
+ * \author Eric MAEKER <eric.maeker@free.fr>
+ * \version 0.6.0
+ * \date 11 May 2011
+ * \class Core::IFirstConfigurationPage
+ * \brief Derive object from this interface and set it inside the PluginManager object pool to get your page in the application configurator.
+ * You should create the pages in the constructor of your plugin. They should be used before the ool initialize(const QStringList &arguments, QString *errorMessage = 0);
+*/
+
+namespace Core {
+
+class CORE_EXPORT IFirstConfigurationPage : public QObject
+{
+    Q_OBJECT
+public:
+    enum Pages {
+        FirstPage = 0,
+        ServerConfig,
+        UserDbConnection,
+        UserCreation,
+        PatientForm,
+        OtherPage,
+        LastPage = 100000
+    };
+    IFirstConfigurationPage(QObject *parent = 0) : QObject(parent) {}
+    virtual ~IFirstConfigurationPage() {}
+
+    virtual int id() const = 0;
+
+    // widget will be deleted after the show
+    virtual QWizardPage *createPage(QWidget *parent) = 0;
+};
+
+} // namespace Core
+
+#endif // IFIRSTCONFIGURATIONPAGE_H
