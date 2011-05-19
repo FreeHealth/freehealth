85d15d5fd0289a1b2b8a39608cafd627fb93abf6
guillaume.denry <guillaume.denry@gmail.com>
2011-05-19 21:36:51 +0000

    hyperlink for hidden calendar items in a month cell
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2203 571bf770-40aa-11de-9a54-b9394989f659

--- libs/utils/widgets/calendar/month_day_widget.cpp
+++ libs/utils/widgets/calendar/month_day_widget.cpp
@@ -12,9 +12,6 @@ MonthDayWidget::MonthDayWidget(AbstractCalendarModel *model, const QDate &day, Q
 
 	m_items = m_model->getItemsBetween(m_day, m_day);
 	qSort(m_items.begin(), m_items.end(), calendarItemLessThan);
-
-	QVBoxLayout *layout = new QVBoxLayout(this);
-	layout->setSpacing(0);
 }
 
 void MonthDayWidget::resizeEvent(QResizeEvent *event) {
@@ -25,25 +22,55 @@ void MonthDayWidget::resizeEvent(QResizeEvent *event) {
 
 void MonthDayWidget::refreshItems() {
 	// delete all
-	QLayoutItem *child;
-	while ((child = layout()->takeAt(0)) != 0) {
-		if (child->widget())
-			delete child->widget();
-		delete child;
+	QList<QWidget*> list;
+	foreach (QObject *obj, children()) {
+		QWidget *widget = qobject_cast<QWidget*>(obj);
+		if (widget)
+			list << widget;
 	}
+	qDeleteAll(list);
 
 	if (!m_items.count())
 		return;
 
-	QVBoxLayout *vlayout = qobject_cast<QVBoxLayout*>(layout());
+	QFont itemFont = QLabel().font(); // the font is a QLabel font classical => todo : change it and choose a better one
+	QFont linkFont = itemFont;
+	linkFont.setUnderline(true);
+	int itemHeight = QFontMetrics(itemFont).height();
+	int visibleItemsCount = this->height() / itemHeight;
+	int top = 0;
+	int count = visibleItemsCount < m_items.count() ? visibleItemsCount - 1 : m_items.count();
 
-	foreach (const CalendarItem &item, m_items) {
-		vlayout->addWidget(new QLabel(QString("<b>%1</b> %2").arg(item.beginning().time().toString("hh:mm")).arg(item.title().isEmpty() ? "(untitled)" : item.title())));
+	for (int i = 0; i < count; i++) {
+		const CalendarItem &item = m_items[i];
+		QLabel *label = new QLabel(QString("<b>%1</b> %2").arg(item.beginning().time().toString("hh:mm")).arg(item.title().isEmpty() ? "(untitled)" : item.title()), this);
+		label->setFont(itemFont);
+		label->move(0, top);
+		label->show();
+		top += itemHeight;
 	}
-	vlayout->addStretch();
 
+	if (count != m_items.count()) { // add a link
+		QLabel *label = new QLabel(tr("see others"), this);
+		label->setFont(linkFont);
+		QPalette palette = label->palette();
+		palette.setColor(QPalette::WindowText, Qt::darkBlue);
+		label->setPalette(palette);
+		label->setCursor(Qt::PointingHandCursor);
+		label->move(0, top);
+		label->show();
+/*		painter.drawText(dayRect.adjusted(2, top, -2, 0), Qt::AlignLeft, "more elements");*/
+	}
+
+/*
+	foreach (const CalendarItem &item, m_items) {
+		QLabel *label = new QLabel(QString("<b>%1</b> %2").arg(item.beginning().time().toString("hh:mm")).arg(item.title().isEmpty() ? "(untitled)" : item.title()), this);
+		label->move(0, top);
+		top += QFontMetrics(label->font()).height();
+		label->show();
+	}
+*/
 /*	if (count != items.count()) { // add a link
 		painter.drawText(dayRect.adjusted(2, top, -2, 0), Qt::AlignLeft, "more elements");
 		}*/
-
 }
