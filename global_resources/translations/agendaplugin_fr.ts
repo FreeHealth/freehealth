5bc8ed311c8b802949dd3155aa8df85a71efb4a4
eric.maeker <eric.maeker@gmail.com>
2011-06-18 18:44:45 +0000

    Agenda: - TS files
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2346 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/agendaplugin_fr.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>AgendaBase</name>
+    <message>
+        <location filename="../../plugins/agendaplugin/agendabase.cpp" line="253"/>
+        <source>Trying to create empty database. 
+       Location : %1 
+       FileName: %2
+       Driver: %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
