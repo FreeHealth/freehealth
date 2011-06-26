e482425c2a67715aefa6f50951756fc4e5aacfec
eric.maeker <eric.maeker@gmail.com>
2011-06-26 13:48:08 +0000

    Calendar/Agenda: - ooopppsss ;)
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2386 571bf770-40aa-11de-9a54-b9394989f659

--- libs/calendar/usercalendar_editor_widget.h
+++ libs/calendar/usercalendar_editor_widget.h
@@ -1,5 +1,35 @@
-#ifndef UserCalendarEditorWidget_H
-#define UserCalendarEditorWidget_H
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2011 by Eric MAEKER, MD (France) <eric.maeker@free.fr>        *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developpers :                                                    *
+ *       Guillaume Denry <guillaume.denry@gmail.com>                       *
+ *       Eric MAEKER, MD <eric.maeker@gmail.com>                           *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADRESS>                                                *
+ ***************************************************************************/
+#ifndef USERCALENDARWIDGET_H
+#define USERCALENDARWIDGET_H
+
+#include <calendar/calendar_exporter.h>
+#include <calendar/usercalendar.h>
 
 #include <QWidget>
 
@@ -11,7 +41,7 @@ namespace Ui {
     class UserCalendarEditorWidget;
 }
 
-class UserCalendarEditorWidget : public QWidget
+class CALENDAR_EXPORT UserCalendarEditorWidget : public QWidget
 {
     Q_OBJECT
 
