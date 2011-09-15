c7d3d97bf3915c5807420cee32c73748226984d5
eric.maeker <eric.maeker@gmail.com>
2011-09-15 12:24:23 +0000

    Patient: - correcting country for virtuals
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2604 571bf770-40aa-11de-9a54-b9394989f659

--- plugins/patientbaseplugin/patientbaseplugin.cpp
+++ plugins/patientbaseplugin/patientbaseplugin.cpp
@@ -138,19 +138,19 @@ void PatientBasePlugin::extensionsInitialized()
         int userLkId = 1; //userModel()->practionnerLkIds(userModel()->currentUserData(Core::IUser::Uuid).toString()).at(0);
 
         QString uid = "b04936fafccb4174a7a6af25dd2bb71c";
-        patientBase()->createVirtualPatient("KIRK", "", "James Tiberius", "M", 6, QDate(1968, 04, 20), "USA", "USS Enterprise",
+        patientBase()->createVirtualPatient("KIRK", "", "James Tiberius", "M", 6, QDate(1968, 04, 20), "US", "USS Enterprise",
                       "21, StarFleet Command", "1968", "EarthTown", uid, userLkId, path+"captainkirk.jpg");
 
         uid = "2c49299b9b554300b46a6e3ef6d40a65";
-        patientBase()->createVirtualPatient("PICARD", "", "Jean-Luc", "M", 6, QDate(1948, 04, 20), "USA", "USS Enterprise-D",
+        patientBase()->createVirtualPatient("PICARD", "", "Jean-Luc", "M", 6, QDate(1948, 04, 20), "US", "USS Enterprise-D",
                       "21, StarFleet Command", "1968", "EarthTown", uid, userLkId, path+"captainpicard.png");
 
         uid = "ef97f37361824b6f826d5c9246f9dc49";
-        patientBase()->createVirtualPatient("ARCHER", "", "Jonathan", "M", 6, QDate(1928, 04, 20), "USA", "Enterprise (NX-01) commanding officer",
+        patientBase()->createVirtualPatient("ARCHER", "", "Jonathan", "M", 6, QDate(1928, 04, 20), "US", "Enterprise (NX-01) commanding officer",
                       "21, StarFleet Command", "1968", "EarthTown", uid, userLkId, path+"captainarcher.jpg");
 
         uid = "493aa06a1b8745b2ae6c79c531ef12a0";
-        patientBase()->createVirtualPatient("JANEWAY", "", "Kathryn", "F", 6, QDate(1938, 04, 20), "USA", "USS Voyager",
+        patientBase()->createVirtualPatient("JANEWAY", "", "Kathryn", "F", 6, QDate(1938, 04, 20), "US", "USS Voyager",
                       "21, StarFleet Command", "1968", "EarthTown", uid, userLkId, path+"captainjaneway.jpg");
 
         PatientModel::activeModel()->refreshModel();
