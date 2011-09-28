fc89918dd8060824b89defdc27e1b5b662a33bad
eric.maeker <eric.maeker@gmail.com>
2011-09-28 06:47:43 +0000

    ZipCodes plugin translations
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2690 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/zipcodesplugin_de.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>ZipCodes::ZipCountryCompleters</name>
+    <message>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="311"/>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="312"/>
+        <source>No country selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="328"/>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="329"/>
+        <source>Zip/city/country association checked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="333"/>
+        <location filename="../../plugins/zipcodesplugin/zipcodescompleters.cpp" line="334"/>
+        <source>Wrong zip/city/country association</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
