76cf5d1916d28891fcabfbce21d03feb3ed033e2
eric.maeker <eric.maeker@gmail.com>
2011-11-07 19:03:33 +0000

    adding a new script to help win32 compilation
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@2904 571bf770-40aa-11de-9a54-b9394989f659

---
+++ scripts/win_release.bat
@@ -0,0 +1,7 @@
+cd ..
+cd glo*/tr*
+lrelease *.ts
+cd ../..
+qmake.exe E:/%1-%2/freediams.pro -r -spec win32-g++ CONFIG+=release LOWERED_APPNAME=%1 CONFIG-=debug_and_release
+mingw32-make.exe -w -j1
+mingw32-make.exe install
