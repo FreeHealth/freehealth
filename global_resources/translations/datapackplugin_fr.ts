35d22f7f65b8160d142e6b436d8816592c6a6311
eric.maeker <eric.maeker@gmail.com>
2012-02-13 20:04:45 +0000

    DataPack Plugin: - translations
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3362 571bf770-40aa-11de-9a54-b9394989f659

---
+++ global_resources/translations/datapackplugin_fr.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>DataPackPlug</name>
+    <message>
+        <location filename="../../plugins/datapackplugin/constants.h" line="34"/>
+        <source>Data pack manager</source>
+        <translation>Gestionnaire des paquets de données</translation>
+    </message>
+</context>
+<context>
+    <name>DataPackPlugin::Internal::DataPackPluginIPlugin</name>
+    <message>
+        <location filename="../../plugins/datapackplugin/datapackplugin.cpp" line="109"/>
+        <source>Initializing DataPackPlugin...</source>
+        <translation>Initialisation du gestionnaire des paquets de données...</translation>
+    </message>
+</context>
+<context>
+    <name>DataPackPlugin::Internal::DataPackPreference</name>
+    <message>
+        <location filename="../../plugins/datapackplugin/datapackpreference.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
