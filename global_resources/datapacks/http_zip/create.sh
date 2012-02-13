d1eb52e5df12217fdbc89b73bb3de12bf72cc0ad
guillaume.denry <guillaume.denry@gmail.com>
2012-02-13 22:21:13 +0000

    backup extension is needed by the BSD sed
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3379 571bf770-40aa-11de-9a54-b9394989f659

--- global_resources/datapacks/http_zip/create.sh
+++ global_resources/datapacks/http_zip/create.sh
@@ -6,10 +6,16 @@
 
 sys=`uname -s`
 if [ "$sys" == "Linux" ] ; then
-    md5cmd="md5sum"
+    md5="md5sum"
+    statarg="-c%s"
+    sedbak=""
 elif [ "$sys" == "Darwin" ] ; then
-    md5cmd="md5"
+    md5="md5"
+    statarg="-s"
+    sedbak=".bak"
 fi
+
+echo "md5 command will be $md5"
 
 # Get all xml files excepted server.conf.xml
 for xmlfile in `find . -type f \( -iname "*.xml" ! -iname "server.conf.xml" \)`
@@ -18,16 +24,14 @@ do
     zipfile=`ls $zipdir/*.zip | head -1` # we assume that the first zip file in the directory is the good one
 
     # Compute data to insert
-    zipmd5=$($md5cmd $zipfile | cut -d ' ' -f 1)
-    if [ "$sys" == "Linux" ] ; then
-        zipsize=$(stat -c%s "$zipfile")
-    elif [ "$sys" == "Darwin" ] ; then
-        zipsize=$(stat -s "$zipfile")
-    fi
+    zipmd5=$($md5 $zipfile | cut -d ' ' -f 1)
+    zipsize=$(stat $statarg "$zipfile")
+
+    echo "For $zipfile, md5=$zipmd5 and size=$zipsize"
 
     # Proceed to the replacement
-    sed -i "s/<size>.*<\/size>/<size>$zipsize<\/size>/g" $xmlfile
-    sed -i "s/<md5>.*<\/md5>/<md5>$zipmd5<\/md5>/g" $xmlfile
+    sed -i $sedbak "s/<size>.*<\/size>/<size>$zipsize<\/size>/g" $xmlfile
+    sed -i $sedbak "s/<md5>.*<\/md5>/<md5>$zipmd5<\/md5>/g" $xmlfile
 done
 
 # Create the zipped server configuration
