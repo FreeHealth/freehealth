54508835f1b5cdfeed053617646e70caa46956f1
guillaume.denry <guillaume.denry@gmail.com>
2012-02-13 21:44:38 +0000

    replace <size> and <md5> tags with true zip values
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3375 571bf770-40aa-11de-9a54-b9394989f659

--- global_resources/datapacks/http_zip/create.sh
+++ global_resources/datapacks/http_zip/create.sh
@@ -4,6 +4,21 @@
 # Get size of pack files
 # Replace in pack description files
 
+# Get all xml files excepted server.conf.xml
+for xmlfile in `find . -type f \( -iname "*.xml" ! -iname "server.conf.xml" \)`
+do
+    zipdir=`dirname $xmlfile`
+    zipfile=`ls $zipdir/*.zip | head -1`
+
+    # Compute data to insert
+    zipmd5=$(md5sum $zipfile | cut -d ' ' -f 1)
+    zipsize=$(stat -c%s "$zipfile")
+
+    # Proceed to the replacement
+    sed -i "s/<size>.*<\/size>/<size>$zipsize<\/size>/g" $xmlfile
+    sed -i "s/<md5>.*<\/md5>/<md5>$zipmd5<\/md5>/g" $xmlfile
+done
+
 # Create the zipped server configuration
 rm serverconf.zip
 zip serverconf.zip server.conf.xml icd10/icd10.xml drugs/all/drugs.xml drugs/fr-only/drugs.xml
