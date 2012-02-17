50cdb476d552646419bdd901b323c23c35da7c0c
eric.maeker <eric.maeker@gmail.com>
2012-02-17 15:51:38 +0000

    scripts: - adding all xmlfiles to zip
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3418 571bf770-40aa-11de-9a54-b9394989f659

--- global_resources/datapacks/http_zip/create.sh
+++ global_resources/datapacks/http_zip/create.sh
@@ -17,8 +17,9 @@ fi
 
 echo "md5 command will be $md5"
 
+xmlfiles=`find . -type f \( -iname "*.xml" ! -iname "server.conf.xml" \)`
 # Get all xml files excepted server.conf.xml
-for xmlfile in `find . -type f \( -iname "*.xml" ! -iname "server.conf.xml" \)`
+for xmlfile in $xmlfiles
 do
     zipdir=`dirname $xmlfile`
     zipfile=`ls $zipdir/*.zip | head -1` # we assume that the first zip file in the directory is the good one
@@ -40,6 +41,6 @@ done
 
 # Create the zipped server configuration
 rm serverconf.zip
-zip serverconf.zip server.conf.xml icd10/icd10.xml drugs/all/drugs.xml drugs/fr-only/drugs.xml
+zip serverconf.zip server.conf.xml $xmlfiles
 
 exit 0
