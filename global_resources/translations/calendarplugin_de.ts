f2c022d5d8c7183dd02b570a74f5f47f47cb215d
christian.a.reiter <christian.a.reiter@gmail.com>
2012-03-18 10:58:44 +0000

    German translation
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3739 571bf770-40aa-11de-9a54-b9394989f659

--- global_resources/translations/calendarplugin_de.ts
+++ global_resources/translations/calendarplugin_de.ts
@@ -6,39 +6,39 @@
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="19"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Heute</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="27"/>
         <source>Day</source>
-        <translation type="unfinished"></translation>
+        <translation>Tag</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="28"/>
         <source>Week</source>
-        <translation type="unfinished"></translation>
+        <translation>Woche</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="29"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Monat</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="133"/>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="135"/>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="137"/>
         <source>d MMM yyyy</source>
-        <translation type="unfinished"></translation>
+        <translation>d.MMM.yyyy</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="135"/>
         <source>d MMM</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">d.MMM.</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/calendar_navbar.cpp" line="137"/>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation>d</translation>
     </message>
 </context>
 <context>
@@ -46,12 +46,12 @@
     <message>
         <location filename="../../plugins/calendarplugin/month_view.cpp" line="107"/>
         <source>d MMM</source>
-        <translation type="unfinished"></translation>
+        <translation>d.MMM.</translation>
     </message>
     <message>
         <location filename="../../plugins/calendarplugin/month_view.cpp" line="109"/>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation>d</translation>
     </message>
 </context>
 </TS>
