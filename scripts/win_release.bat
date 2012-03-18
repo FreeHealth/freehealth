f8390814366653dd029300179f4c8cf499bf8c09
eric.maeker <eric.maeker@gmail.com>
2012-03-18 15:59:52 +0000

    Win32 correction
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3750 571bf770-40aa-11de-9a54-b9394989f659

--- scripts/win_release.bat
+++ scripts/win_release.bat
@@ -1,7 +1,7 @@
 cd ..
-cd glo*/tr*
+cd global_resources/translations
 lrelease *.ts
-cd ../..
-qmake.exe E:/%1-%2/%1.pro -r -spec win32-g++ CONFIG+=release LOWERED_APPNAME=%1 CONFIG-=debug_and_release
+cd ../../%1
+qmake.exe %1.pro -r -spec win32-g++ CONFIG+=release LOWERED_APPNAME=%1 CONFIG-=debug_and_release
 mingw32-make.exe -w
 mingw32-make.exe install
