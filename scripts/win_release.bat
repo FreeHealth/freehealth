9c24ee6dc3953cc03df3637d92ddb081d8bb13d9
eric.maeker <eric.maeker@gmail.com>
2012-03-18 01:57:05 +0000

    no jobs
    git-svn-id: https://freemedforms.googlecode.com/svn/trunk@3715 571bf770-40aa-11de-9a54-b9394989f659

--- scripts/win_release.bat
+++ scripts/win_release.bat
@@ -3,5 +3,5 @@ cd glo*/tr*
 lrelease *.ts
 cd ../..
 qmake.exe E:/%1-%2/%1.pro -r -spec win32-g++ CONFIG+=release LOWERED_APPNAME=%1 CONFIG-=debug_and_release
-mingw32-make.exe -w -j1
+mingw32-make.exe -w
 mingw32-make.exe install
