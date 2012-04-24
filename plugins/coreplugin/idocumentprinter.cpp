98b42f98af208cb22ae69f76be4efd6813a9df1d
eric.maeker <eric.maeker@gmail.com>
2012-04-24 16:20:57 +0200

    adding print doxgen documentation

--- plugins/coreplugin/idocumentprinter.cpp
+++ plugins/coreplugin/idocumentprinter.cpp
@@ -29,10 +29,27 @@
   \class Core::IDocumentPrinter
   \brief This class is a Document Printer's manager.
   The printerplugin will create one instance of it and leave it in the pluginmanager pool.
-  Get it, and use it for your printing. But remember to never delete it.
-  If in user settings, PDF cached is enabled, you can retrieve all printed docs in the QList<Doc>.
+  Get it, and use it for your printing. But remember to \b never delete it.
+  If in user settings, PDF cached is enabled, you can retrieve all printed docs in the Core::IDocumentPrinter::printedDocs().
+
+  You can define the headers, watermark and footer to use in the Core::IDocumentPrinter::print() function.
+
+  The tokens can be:
+  - global (used for all documents during the print process (header, footer, watermark and document itself)
+  - specific to one document: header, footer, watermark and document itself.
+  - set this specificities in the Core::IDocumentPrinter::addTokens()
+
+  The print process shows a dialog to the user. The user can modify some params of the print process:
+  - select a printer
+  - select a 2 n-up printing or not
+  - print or not duplicate
+  You should not care about these params.
 
   \code
+  // Include files
+  #include <coreplugin/idocumentprinter>
+  #include <extensionsystem/pluginmanager.h>
+
   // Get the IDocumentPrinter object from objects pool
   inline static Core::IDocumentPrinter *printer() {return ExtensionSystem::PluginManager::instance()->getObject<Core::IDocumentPrinter>();}
 
@@ -53,7 +70,49 @@
   }
   \endcode
   \todo create a queue that manages printing jobs
+  \todo add a addUserTokens(), addPatientTokens() to automatically include the user && patient related tokens (extracted from Core::IUser, Core::IPatient).
 */
+
+/** \fn virtual void Core::IDocumentPrinter::clearTokens() = 0;
+  Clear all registered tokens.
+ */
+
+/** \fn virtual void Core::IDocumentPrinter::addTokens(const int tokenWhere, const QHash<QString, QVariant> &tokensAndValues) = 0;
+  Register tokens \e tokensAndValues for a specific document, or a global usage with \e tokenWhere.
+  Use the Core::IDocumentPrinter::TokensWhere enumerator to define the \e tokenWhere value.
+  The tokens are represented in a hash of QString (key), QVariant (value).
+  For more informations about the token management and usage please refer to the web site:
+  - French: http://www.freemedforms.com/fr/manuals/freemedforms/tokenmanager
+  - English: http://www.freemedforms.com/en/manuals/freemedforms/tokenmanager
+ */
+
+/** \fn virtual bool Core::IDocumentPrinter::print(const QTextDocument &text, const int papers = Papers_Generic_User, bool printDuplicata = false) const = 0;
+  Prints a QTextDocument \e text using a set of user papers (header, footer, watermark) \e papers. Use the enumerator Core::IDocumentPrinter::PapersToUse to define the /e papers.
+  You can automatically print a duplicate.
+ */
+
+/** \fn virtual bool Core::IDocumentPrinter::print(QTextDocument *text, const int papers = Papers_Generic_User, bool printDuplicata = false) const = 0;
+  Prints a QTextDocument \e text using a set of user papers (header, footer, watermark) \e papers. Use the enumerator Core::IDocumentPrinter::PapersToUse to define the /e papers.
+  You can automatically print a duplicate.
+ */
+
+/** \fn virtual bool Core::IDocumentPrinter::print(const QString &html, const int papers = Papers_Generic_User, bool printDuplicata = false) const = 0;
+  Prints an HTML \e text using a set of user papers (header, footer, watermark) \e papers. Use the enumerator Core::IDocumentPrinter::PapersToUse to define the /e papers.
+  You can automatically print a duplicate.
+ */
+
+/** \fn virtual bool Core::IDocumentPrinter::printPreview(const QString &html, const int papers = Papers_Generic_User, bool printDuplicata = false) const = 0;
+  Show a print preview. \sa print()
+ */
+
+/** \fn static void Core::IDocumentPrinter::addPrintedDoc(const QString &fileName, const QString &docName, const QDateTime &dt, const QString &userUid);
+  You can use a PDF cache system. This cache system is automatically managed by the print process.
+ */
+
+/** \fn static Core::IDocumentPrinter::QList<PrintedDocumentTracer> printedDocs()
+  You can use a PDF cache system. This cache system is automatically managed by the print process.
+ */
+
 
 using namespace Core;
 
