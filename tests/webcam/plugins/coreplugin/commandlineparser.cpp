19371332fe3805fd0a570d7ec018fc06e32daad2
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-05-22 05:55:31 +0200

    refactored 2 TODOs

--- tests/webcam/plugins/coreplugin/commandlineparser.cpp
+++ tests/webcam/plugins/coreplugin/commandlineparser.cpp
@@ -62,14 +62,14 @@ void CommandLine::feedPatientDatas(Core::IPatient *patient)
 
 QVariant CommandLine::value(int param, const QVariant &def) const
 {
-    /** \todo code heree */
+    //TODO: CommandLine::value() - code here
     return QVariant();
 //    return d->value.value(param,def);
 }
 
 QString CommandLine::paramName(int param) const
 {
-    /** \todo code heree */
+    //TODO: CommandLine::paramName() - code here
 //    if (d->params.keys().contains(param))
 //        return d->params.value(param);
 //    else
