0247d3901b0da21cfd0dd063a34bbaba0de1d95b
Pierre-Marie Desombre <pm.desombre@gmail.com>
2012-06-02 08:49:09 +0200

    freeaccount : revert commit

--- plugins/accountbaseplugin/accountbase.cpp
+++ plugins/accountbaseplugin/accountbase.cpp
@@ -168,10 +168,9 @@ AccountBase::AccountBase(QObject *parent)
     addField(Table_MedicalProcedure, MP_ABSTRACT,       "ABSTRACT",       FieldIsLongText);
     addField(Table_MedicalProcedure, MP_TYPE,           "TYPE",           FieldIsShortText);
     addField(Table_MedicalProcedure, MP_AMOUNT,         "AMOUNT",         FieldIsReal);
-    addField(Table_MedicalProcedure, MP_OTHERS,         "OTHERS",         FieldIsBlob);
+    addField(Table_MedicalProcedure, MP_REIMBOURSEMENT, "REIMBOURSEMENT", FieldIsReal);
     addField(Table_MedicalProcedure, MP_DATE,           "DATE",           FieldIsDate);
-    /** Add a link to INSURANCE table ? One MP is reimburse by a specific insurance... */
-    //ok but have to enter insurance uid in .csv
+    //addField(Table_MedicalProcedure, MP_OTHERS,         "OTHERS",         FieldIsBlob);
 
 //    "CREATE TABLE 	actes_disponibles ("  --> medical_procedure
 //            "id_acte_dispo  int(10)  	UNSIGNED  	       		NOT NULL  	 auto_increment ,"
