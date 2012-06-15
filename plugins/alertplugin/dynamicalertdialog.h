32dac47a62fefda5756653dd19fd8fc2a67853d3
eric.maeker <eric.maeker@gmail.com>
2012-06-15 07:42:16 +0200

    some corrections

--- plugins/alertplugin/dynamicalertdialog.h
+++ plugins/alertplugin/dynamicalertdialog.h
@@ -71,6 +71,7 @@ public:
 
     static DynamicAlertResult executeDynamicAlert(const AlertItem &item, const QString &themedIcon = QString::null, QWidget *parent = 0);
     static DynamicAlertResult executeDynamicAlert(const QList<AlertItem> &item, const QString &themedIcon = QString::null, QWidget *parent = 0);
+    static DynamicAlertResult executeDynamicAlert(const QList<AlertItem> &item, const QList<QAbstractButton*> &buttons, const QString &themedIcon = QString::null, QWidget *parent = 0);
 
 private Q_SLOTS:
     void override();
