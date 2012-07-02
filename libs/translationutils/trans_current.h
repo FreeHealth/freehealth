475b56fd85a64ff7672f86c2f317109afc05d9d6
eric.maeker <eric.maeker@gmail.com>
2012-07-03 01:09:33 +0200

    new trans

--- libs/translationutils/trans_current.h
+++ libs/translationutils/trans_current.h
@@ -68,6 +68,7 @@ const char* const VALIDATE                           = QT_TRANSLATE_NOOP("tkCons
 const char* const OVERRIDE                           = QT_TRANSLATE_NOOP("tkConstants", "Override");
 const char* const EDIT_ALERT                         = QT_TRANSLATE_NOOP("tkConstants", "Edit alert");
 const char* const REMIND_LATER                       = QT_TRANSLATE_NOOP("tkConstants", "Remind later");
+const char* const _1_ALERTS                           = QT_TRANSLATE_NOOP("tkConstants", "%1 alerts");
 
 const char* const FROM                          = QT_TRANSLATE_NOOP("tkConstants", "from");
 const char* const TO                            = QT_TRANSLATE_NOOP("tkConstants", "to");
