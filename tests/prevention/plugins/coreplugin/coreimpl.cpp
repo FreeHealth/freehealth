21a86dc29344dd5708f4b1645f299d85fa397dd2
eric.maeker <eric.maeker@gmail.com>
2012-07-08 22:15:22 +0200

    remove fakescriptmanager

--- tests/prevention/plugins/coreplugin/coreimpl.cpp
+++ tests/prevention/plugins/coreplugin/coreimpl.cpp
@@ -42,7 +42,7 @@
 #include <coreplugin/constants_icons.h>
 #include <coreplugin/commandlineparser.h>
 #include <coreplugin/ipadtools.h>
-#include <coreplugin/fakescriptmanager.h>
+//#include <coreplugin/fakescriptmanager.h>
 
 #include <translationutils/constanttranslations.h>
 #include <utils/log.h>
@@ -97,7 +97,7 @@ CoreImpl::CoreImpl(QObject *parent) :
     m_CommandLine = new CommandLine;
 //    m_CommandLine->feedPatientDatas(m_Patient);
 
-    m_Script = new FakeScriptManager(this);
+//    m_Script = new FakeScriptManager(this);
 
     setMainWindow(new MainWindow);
 
