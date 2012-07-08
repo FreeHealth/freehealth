ae8b0044635a1600ecfe37fdf60502f97e2f2c3b
eric.maeker <eric.maeker@gmail.com>
2012-07-08 21:11:54 +0200

    adding a "add alert" button at the beginning of the toolbar

--- plugins/alertplugin/alertplaceholdertest.h
+++ plugins/alertplugin/alertplaceholdertest.h
@@ -32,6 +32,7 @@
 #include <alertplugin/alertitem.h>
 #include <QToolBar>
 #include <QPointer>
+#include <QToolButton>
 
 namespace Alert {
 class StaticAlertToolButton;
@@ -65,11 +66,19 @@ public:
     bool containsAlertUuid(const QString &alertUid);
     bool removeAlertUuid(const QString &alertUid);
 
+private Q_SLOTS:
+    void createAlert();
+
+private:
+    void addNewAlertButton();
+    bool eventFilter(QObject *obj, QEvent *event);
+
 private:
     QPointer<QToolBar> _widget;
     QList<AlertItem> alerts;
     QList<int> _priorities;
     QHash<QString, StaticAlertToolButton *> _buttons;
+    QToolButton *_newButton;
 };
 
 }  // namespace Alert
