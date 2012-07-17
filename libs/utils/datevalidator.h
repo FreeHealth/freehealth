796d19349918f0fb18d6838380e815443f7aa36f
eric.maeker <eric.maeker@gmail.com>
2012-07-17 20:48:33 +0200

    doc

--- libs/utils/datevalidator.h
+++ libs/utils/datevalidator.h
@@ -32,6 +32,13 @@
 #include <QStringList>
 #include <QDate>
 
+/**
+ * \file datevalidator.h
+ * \author Eric MAEKER <eric.maeker@gmail.com>
+ * \version 0.8.0
+ * \date 17 July 2012
+*/
+
 namespace Utils {
 
 class UTILS_EXPORT DateValidator : public QValidator
