0516b7b2a3f77431c263463de62b1cb53cd2ba6b
eric.maeker <eric.maeker@gmail.com>
2012-07-27 00:15:42 +0200

    indent

--- libs/calendar/view.cpp
+++ libs/calendar/view.cpp
@@ -99,7 +99,7 @@ QPixmap ViewWidget::generatePixmap()
 {
 	QPixmap pixmap(width(), height());
 	QPainter painter(&pixmap);
-        paintBody(&painter, rect());
+    paintBody(&painter, rect());
 	return pixmap;
 }
 
