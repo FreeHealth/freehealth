eb9e76d619e888a455bc15d6746349f8fd7774d5
eric.maeker <eric.maeker@gmail.com>
2012-09-15 22:31:33 +0200

    code style

--- plugins/icdplugin/icdwidgetmanager.cpp
+++ plugins/icdplugin/icdwidgetmanager.cpp
@@ -50,7 +50,7 @@ using namespace ICD;
 using namespace Internal;
 using namespace Trans::ConstantTranslations;
 
-inline static Core::ActionManager *actionManager() {return Core::ICore::instance()->actionManager();}
+static inline Core::ActionManager *actionManager() {return Core::ICore::instance()->actionManager();}
 static inline Core::ContextManager *contextManager() { return Core::ICore::instance()->contextManager(); }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
