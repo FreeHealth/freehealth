02896f4bef186b6e2c3835cd6746d08327842f80
eric.maeker <eric.maeker@gmail.com>
2012-09-16 17:43:55 +0200

    new objects: FormCore, FormCollection, FormContextualWidgetManager

---
+++ plugins/formmanagerplugin/formcollection.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers: Eric MAEKER, <eric.maeker@gmail.com>                 *
+ *   Contributors:                                                         *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+/*!
+ * \class Form::FormCollection
+ * \brief Holds a book of forms (a specific mode, the central form, duplicates or not...).
+ * The form books are created and managed by the Form::FormManager object. Books contain
+ * all forms available for a specific mode.
+ * \sa Form::FormManager
+ */
+
+#include "formcollection.h"
+#include "iformitem.h"
+
+#include <translationutils/constants.h>
+
+#include <QDebug>
+
+using namespace Form;
+using namespace Internal;
+using namespace Trans::ConstantTranslations;
+
+namespace Form {
+namespace Internal {
+/*!
+ * \class Form::Internal::FormCollectionPrivate
+ * \brief Private implementation of the Form::FormCollection class.
+ */
+class FormCollectionPrivate
+{
+public:
+    FormCollectionPrivate(FormCollection *parent) :
+        q(parent)
+    {
+    }
+    
+    ~FormCollectionPrivate()
+    {
+    }
+    
+public:
+    QList<Form::FormMain *> _emptyRootForms;
+
+private:
+    FormCollection *q;
+};
+}  // namespace Internal
+} // end namespace Form
+
+/*! Constructor of the Form::FormCollection class */
+FormCollection::FormCollection(QObject *parent) :
+    QObject(parent),
+    d(new FormCollectionPrivate(this))
+{
+}
+
+/*! Destructor of the Form::FormCollection class */
+FormCollection::~FormCollection()
+{
+    if (d)
+        delete d;
+    d = 0;
+}
+
+/*! Initializes the object with the default values. Return true if initialization was completed. */
+bool FormCollection::initialize()
+{
+}
+
---
+++ plugins/formmanagerplugin/formcore.h
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers :                                                     *
+ *       Eric MAEKER <eric.maeker@gmail.com>                               *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#ifndef FORM_FORMCORE_H
+#define FORM_FORMCORE_H
+
+#include <formmanagerplugin/formmanager_exporter.h>
+#include <QObject>
+
+/**
+ * \file formcore.h
+ * \author Eric MAEKER
+ * \version 0.8.0
+ * \date 16 Sept 2012
+*/
+
+namespace Form {
+class FormManager;
+//class EpisodeManager;
+namespace Internal {
+class FormManagerPlugin;
+class FormCorePrivate;
+}  // namespace Internal
+
+class FORM_EXPORT FormCore : public QObject
+{
+    Q_OBJECT
+    friend class Form::Internal::FormManagerPlugin;
+    explicit FormCore(QObject *parent = 0);
+
+public:
+    static FormCore &instance();
+    ~FormCore();
+    
+    bool initialize();
+    
+    Form::FormManager &formManager() const;
+//    Form::EpisodeManager &episodeManager() const;
+
+Q_SIGNALS:
+    
+public Q_SLOTS:
+    void activatePatientFileCentralMode();
+    
+private:
+    Internal::FormCorePrivate *d;
+    static FormCore *_instance;
+};
+
+} // namespace Form
+
+#endif  // FORM_FORMCORE_H
+
