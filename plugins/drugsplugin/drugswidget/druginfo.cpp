9bb059d86f6ebd445bb272d9c68a0b1493af5df3
eric.maeker <eric.maeker@gmail.com>
2012-09-16 11:44:07 +0200

    "static inline"

--- plugins/drugsplugin/drugswidget/druginfo.cpp
+++ plugins/drugsplugin/drugswidget/druginfo.cpp
@@ -47,7 +47,7 @@
 using namespace DrugsWidget::Internal;
 using namespace DrugsWidget::Constants;
 
-inline static DrugsDB::DrugsModel *drugModel() { return DrugsWidget::DrugsWidgetManager::instance()->currentDrugsModel(); }
+static inline DrugsDB::DrugsModel *drugModel() { return DrugsWidget::DrugsWidgetManager::instance()->currentDrugsModel(); }
 //static inline DrugsDB::InteractionManager &interactionManager() {return DrugsDB::DrugBaseCore::instance().interactionManager();}
 
 DrugInfo::DrugInfo(const QVariant &drugUid,  QWidget *parent)
