e906dee3be931c43b0da2f693667a45a82fdcc25
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-09-22 13:54:32 +0200

    fixed LOG output

--- freetoolbox/plugins/biblio/bibliobase.cpp
+++ freetoolbox/plugins/biblio/bibliobase.cpp
@@ -118,7 +118,7 @@ bool BiblioBase::createDatabase(const QString &connectionName , const QString &d
         DB = QSqlDatabase::addDatabase("QSQLITE", connectionName);
         if (!QDir(pathOrHostName).exists())
             if (!QDir().mkpath(pathOrHostName))
-                tkTr(Trans::Constants::_1_ISNOT_AVAILABLE_CANNOTBE_CREATED).arg(pathOrHostName);
+                LOG(tkTr(Trans::Constants::_1_ISNOT_AVAILABLE_CANNOTBE_CREATED).arg(pathOrHostName));
         DB.setDatabaseName(QDir::cleanPath(pathOrHostName + QDir::separator() + dbName));
         if (!DB.open())
             LOG(tkTr(Trans::Constants::DATABASE_1_CANNOT_BE_CREATED_ERROR_2).arg(dbName).arg(DB.lastError().text()));
