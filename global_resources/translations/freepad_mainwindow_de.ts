af50d13ed38eb66a88a02b9c5e3fafd33c611c90
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-09-29 19:15:11 +0200

    add translations

---
+++ global_resources/translations/freepad_mainwindow_de.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainWin::Internal::MainWindow</name>
+    <message>
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.ui" line="22"/>
+        <source>Pad sandbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.ui" line="38"/>
+        <source>New token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.ui" line="45"/>
+        <source>Remove token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.ui" line="101"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Lucida Grande&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[Before ~DRUG~ After]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[Prendre ~Q_FROM~][ à ~Q_TO~] [~Q_SCHEME~]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[ ~REPEATED_DAILY_SCHEME~][ ~MEAL~][ tous les [~PERIOD~]~PERIOD_SCHEME~][ pendant ~D_FROM~][ à ~D_TO~][ ~D_SCHEME~]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[Par voie : ~ROUTE~]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[Distribution journalière : ~DISTRIBUTED_DAILY_SCHEME~]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[Intervalle minimum entre les prises : ~MIN_INTERVAL~]&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;[~NOTE~ ]&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.ui" line="134"/>
+        <source>Calendar sandbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWin::MainWindow</name>
+    <message>
+        <location filename="../../freepad/plugins/mainwindowplugin/mainwindow.cpp" line="93"/>
+        <source>Creating Main Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
