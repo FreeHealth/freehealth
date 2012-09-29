7007e07126d11d65a3aa0b637e41528888c7b11f
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-09-29 19:10:48 +0200

    added install wizard shell script for qtcreator template wizards

---
+++ qtcreator_wizards/install-wizards.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+QTCREATOR_CONFIG_PATH="$HOME/.config/Nokia/qtcreator/templates/wizards"
+
+
+if [ ! -e $QTCREATOR_CONFIG_PATH ]; then
+  echo "Creating the QtCreator config path: $QTCREATOR_CONFIG_PATH"
+  mkdir -p $QTCREATOR_CONFIG_PATH
+fi
+
+for i in *; do
+    if [ -d $i ]; then
+        echo "Creating $QTCREATOR_CONFIG_PATH/$i"
+        ln -sf $PWD/$i $QTCREATOR_CONFIG_PATH
+    fi
+done
