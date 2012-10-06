3b0beb9a87d4c29ca5d11371fd110a1f068f0bac
eric.maeker <eric.maeker@gmail.com>
2012-10-06 11:34:09 +0200

    formitemdelegate

---
+++ plugins/formmanagerplugin/formviewdelegate.cpp
@@ -0,0 +1,108 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#include "formviewdelegate.h"
+#include "formtreemodel.h"
+
+#include <coreplugin/icore.h>
+#include <coreplugin/itheme.h>
+#include <coreplugin/constants_icons.h>
+
+#include <QApplication>
+#include <QPainter>
+#include <QModelIndex>
+
+static inline Core::ITheme *theme()  { return Core::ICore::instance()->theme(); }
+
+namespace Form {
+namespace Internal {
+
+FormViewDelegate::FormViewDelegate(QObject *parent) :
+    QStyledItemDelegate(parent),
+    _formTreeModel(0)
+{
+}
+
+void FormViewDelegate::setFormTreeModel(FormTreeModel *model)
+{
+    _formTreeModel = model;
+}
+
+QSize FormViewDelegate::sizeHint(const QStyleOptionViewItem &option,const QModelIndex &index) const
+{
+    const bool topLevel = !index.parent().isValid();
+    if (topLevel) {
+        return QStyledItemDelegate::sizeHint(option, index) + QSize(10,10);
+    }
+    return QStyledItemDelegate::sizeHint(option, index);
+}
+
+void FormViewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
+           const QModelIndex &index) const
+{
+    // Add the fancy button
+    if (option.state & QStyle::State_MouseOver) {
+        if ((QApplication::mouseButtons() & Qt::LeftButton) == 0)
+            pressedIndex = QModelIndex();
+        QBrush brush = option.palette.alternateBase();
+        if (index == pressedIndex)
+            brush = option.palette.dark();
+        painter->fillRect(option.rect, brush);
+    }
+
+    QStyledItemDelegate::paint(painter, option, index);
+
+    // Draw fancy button
+    if (index.column()==FormTreeModel::EmptyColumn1 &&
+            (option.state & QStyle::State_MouseOver)) {
+        QIcon icon;
+        if (option.state & QStyle::State_Selected) {
+            // test the form to be unique or multiple episode
+            if (_formTreeModel->isUniqueEpisode(index))
+                return;
+            if (_formTreeModel->isNoEpisode(index))
+                return;
+            icon = theme()->icon(Core::Constants::ICONADDLIGHT);
+        } else {
+            // test the form to be unique or multiple episode
+            if (_formTreeModel->isUniqueEpisode(index))
+                return;
+            if (_formTreeModel->isNoEpisode(index))
+                return;
+            icon = theme()->icon(Core::Constants::ICONADDDARK);
+        }
+
+        QRect iconRect(option.rect.right() - option.rect.height(),
+                       option.rect.top(),
+                       option.rect.height(),
+                       option.rect.height());
+
+        icon.paint(painter, iconRect, Qt::AlignRight | Qt::AlignVCenter);
+    }
+}
+
+} // namespace Internal
+} // namespace Form
---
+++ plugins/formmanagerplugin/formviewdelegate.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#ifndef FORM_INTERNAL_FORMVIEWDELEGATE_H
+#define FORM_INTERNAL_FORMVIEWDELEGATE_H
+
+#include <QStyledItemDelegate>
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+/**
+ * \file formviewdelegate.h
+ * \author Eric MAEKER
+ * \version 0.8.0
+ * \date 06 Oct 2012
+*/
+
+namespace Form {
+class FormTreeModel;
+namespace Internal {
+
+class FormViewDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    FormViewDelegate(QObject *parent = 0);
+    void setFormTreeModel(FormTreeModel *model);
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
+               const QModelIndex &index) const;
+
+    QSize sizeHint(const QStyleOptionViewItem &option,
+                   const QModelIndex &index) const;
+
+public:
+    mutable QModelIndex pressedIndex;
+    FormTreeModel *_formTreeModel;
+};
+
+} // namespace Internal
+} // namespace Form
+
+#endif // FORM_INTERNAL_FORMVIEWDELEGATE_H
