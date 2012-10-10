f2fe816900a2413213ce3b80a405b5e3fc3bd394
eric.maeker <eric.maeker@gmail.com>
2012-10-10 21:11:25 +0200

    license header

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
-
-# This script is part of FreeMedForms project : http://www.freemedforms.com
-# (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>
-#
-# This script will prepare the source package for freediams only
-# Documentations must installed by hand before
-#
+#/***************************************************************************
+# *  The FreeMedForms project is a set of free, open source medical         *
+# *  applications.                                                          *
+# *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+# *  All rights reserved.                                                   *
+# *                                                                         *
+# *  This program is free software: you can redistribute it and/or modify   *
+# *  it under the terms of the GNU General Public License as published by   *
+# *  the Free Software Foundation, either version 3 of the License, or      *
+# *  (at your option) any later version.                                    *
+# *                                                                         *
+# *  This program is distributed in the hope that it will be useful,        *
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+# *  GNU General Public License for more details.                           *
+# *                                                                         *
+# *  You should have received a copy of the GNU General Public License      *
+# *  along with this program (COPYING.FREEMEDFORMS file).                   *
+# *  If not, see <http://www.gnu.org/licenses/>.                            *
+# ***************************************************************************/
+#/***************************************************************************
+# *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+# *   Contributors :                                                        *
+# *       NAME <MAIL@ADDRESS.COM>                                           *
+# *       NAME <MAIL@ADDRESS.COM>                                           *
+# ***************************************************************************/
 
 BUNDLE_NAME=""
 GIT_REVISION=""
