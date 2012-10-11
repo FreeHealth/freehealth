8b7ab632f5f3944525cbeb20a687de5e0d0808de
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-10-11 14:37:41 +0200

    consolidate/fix some strings + var name

--- plugins/coreplugin/dialogs/pluginaboutpage.cpp
+++ plugins/coreplugin/dialogs/pluginaboutpage.cpp
@@ -98,7 +98,7 @@ QWidget *PluginAboutPage::createPage(QWidget *parent)
     new QTreeWidgetItem(i, QStringList() << tkTr(Trans::Constants::VERSION) + " " + m_Spec->version() );
     new QTreeWidgetItem( i, QStringList() << tkTr(Trans::Constants::BUILD_DATE_TIME_1_2).arg(__DATE__, __TIME__));
     if (Utils::isDebugWithoutInstallCompilation()) {
-        new QTreeWidgetItem( i, QStringList() << tkTr(Trans::Constants::BUILD_DEBUG) + "no install");
+        new QTreeWidgetItem( i, QStringList() << tkTr(Trans::Constants::BUILD_DEBUG) + " - no install");
     } else {
         new QTreeWidgetItem( i, QStringList() << tkTr(Trans::Constants::BUILD_RELEASE) );
     }
