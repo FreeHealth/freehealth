a67542f8e4881393a49484a8c664b078976d72e5
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-10-17 03:02:15 +0200

    simplify WorkingPlacesModel no QAbstractItemModel inheritance; inherits now directly from QSqlTableModel; simpler code, saves memory, more compliant to QSqlTableModel API, etc.

--- plugins/accountbaseplugin/workingplacesmodel.cpp
+++ plugins/accountbaseplugin/workingplacesmodel.cpp
@@ -52,17 +52,11 @@ namespace Internal {
 class WorkingPlacesModelPrivate
 {
 public:
-    WorkingPlacesModelPrivate(WorkingPlacesModel *parent) : m_SqlTable(0), q(parent)
+    WorkingPlacesModelPrivate(WorkingPlacesModel *parent) : q(parent)
     {
-        m_SqlTable = new QSqlTableModel(q,QSqlDatabase::database(Constants::DB_ACCOUNTANCY));
-        m_SqlTable->setTable(AccountBase::instance()->table(Constants::Table_Sites));
     }
 
     ~WorkingPlacesModelPrivate () {}
-
-public:
-    QSqlTableModel *m_SqlTable;
-
 
 private:
     WorkingPlacesModel *q;
@@ -73,10 +67,12 @@ private:
 
 
 WorkingPlacesModel::WorkingPlacesModel(QObject *parent) :
-        QAbstractTableModel(parent), d(new Internal::WorkingPlacesModelPrivate(this))
+    QSqlTableModel(parent, QSqlDatabase::database(Constants::DB_ACCOUNTANCY)),
+    d(new Internal::WorkingPlacesModelPrivate(this))
 {
-    d->m_SqlTable->setEditStrategy(QSqlTableModel::OnFieldChange);
-    d->m_SqlTable->select();
+    setTable(AccountBase::instance()->table(Constants::Table_Sites));
+    setEditStrategy(QSqlTableModel::OnFieldChange);
+    select();
 }
 
 WorkingPlacesModel::~WorkingPlacesModel()
@@ -87,36 +83,15 @@ WorkingPlacesModel::~WorkingPlacesModel()
     }
 }
 
-
-int WorkingPlacesModel::rowCount(const QModelIndex &parent) const
-{
-    int rows = 0;
-    d->m_SqlTable->setFilter("");
-    d->m_SqlTable->select();
-    rows = d->m_SqlTable->rowCount(parent);
-    return rows;
-}
-
-int WorkingPlacesModel::columnCount(const QModelIndex &parent) const
-{
-    return d->m_SqlTable->columnCount(parent);
-}
-
-
 QVariant WorkingPlacesModel::data(const QModelIndex &index, int role) const
 {
     if (!index.isValid()) {
-       return QVariant();
+        return QVariant();
     }
     if (role == Qt::DisplayRole || role == Qt::EditRole) {
-        return d->m_SqlTable->data(index, role);
+        return QSqlTableModel::data(index, role);
     }
     return QVariant();
-}
-
-bool WorkingPlacesModel::setData(const QModelIndex &index, const QVariant &value, int role)
-{
-    return d->m_SqlTable->setData(index, value, role);
 }
 
 
@@ -127,43 +102,3 @@ QVariant WorkingPlacesModel::headerData(int section, Qt::Orientation orientation
     Q_UNUSED(role);
     return QVariant();
 }
-
-bool WorkingPlacesModel::insertRows(int row, int count, const QModelIndex &parent)
-{
-      bool ret = d->m_SqlTable->insertRows(row, count, parent);
-      return ret;
-}
-
-bool WorkingPlacesModel::removeRows(int row, int count, const QModelIndex &parent)
-{
-    return d->m_SqlTable->removeRows(row, count, parent);
-}
-
-bool WorkingPlacesModel::isDirty() const
-{
-    return true;
-}
-
-bool WorkingPlacesModel::submit()
-{
-    if (d->m_SqlTable->submitAll()) {
-        return true;
-    }
-    return false;
-}
-
-void  WorkingPlacesModel::revert()
-{
-    d->m_SqlTable->revert();
-}
-
-QSqlError WorkingPlacesModel::lastError(){
-    return d->m_SqlTable->lastError();
-}
-
-void WorkingPlacesModel::setFilter(const QString & filter)
-{
-    d->m_SqlTable->setFilter(filter);
-    d->m_SqlTable->select();
-}
-
