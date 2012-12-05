fd7572a3c10542e2c0ee9055e881062cab24c20a
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-12-05 12:26:52 +0100

    consolidation of redundance

--- plugins/patientbaseplugin/patientmodel.cpp
+++ plugins/patientbaseplugin/patientmodel.cpp
@@ -471,9 +471,7 @@ QVariant PatientModel::data(const QModelIndex &index, int role) const
         case IPatient::Photo_32x32 :
         {
             QPixmap pix = d->getPatientPhoto(index);
-            if (pix.isNull())
-                return pix;
-            if (pix.size()==QSize(32,32)) {
+            if (pix.isNull() || pix.size() == QSize(32,32)) {
                 return pix;
             }
             return pix.scaled(QSize(32,32));
@@ -481,9 +479,7 @@ QVariant PatientModel::data(const QModelIndex &index, int role) const
         case IPatient::Photo_64x64 :
         {
             QPixmap pix = d->getPatientPhoto(index);
-            if (pix.isNull())
-                return pix;
-            if (pix.size()==QSize(64,64)) {
+            if (pix.isNull() || pix.size() == QSize(64,64)) {
                 return pix;
             }
             return pix.scaled(QSize(64,64));
