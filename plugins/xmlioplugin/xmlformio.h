eed0eacc3f1929ff698899ae45ca14072a87d114
Christian A. Reiter <christian.a.reiter@gmail.com>
2012-12-14 12:11:23 +0100

    Revert "better var names" - wrong commit
    This reverts commit 2e32cc2e7985a02eef3ef62a2b73cc932bf96b3b.

--- plugins/xmlioplugin/xmlformio.h
+++ plugins/xmlioplugin/xmlformio.h
@@ -93,7 +93,7 @@ private:
     bool createCategory(const QDomElement &element, Category::CategoryItem *parent, const QString &readingAbsPathFile) const;
 
 public:
-    QList<Utils::GenericUpdateInformation> checkDatabaseFormFileForUpdates() const;
+    bool checkDatabaseFormFileForUpdates() const;
 
 private:
      mutable QString m_AbsFileName;
