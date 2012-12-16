2e03ad7f5d8fa69a55c8908fcca042e41447d8c1
eric.maeker <eric.maeker@gmail.com>
2012-12-16 09:40:50 +0100

    doc

--- plugins/usermanagerplugin/widgets/iuserviewerpage.cpp
+++ plugins/usermanagerplugin/widgets/iuserviewerpage.cpp
@@ -26,7 +26,45 @@
  ***************************************************************************/
 
 /**
-  \class UserPlugin::IUserViewerPage
-  \todo document
+ * \class UserPlugin::IUserViewerPage
+ * Acts like a QWizardPage in the UserPlugin::UserViewer. Each gets
+ * included in the menu and in its central view. \n
+ * The UserPlugin::UserViewer manages the UserPlugin::IUserViewerWidget
+ * objects (that are extracted from the pluginmanager object pool).
 */
+
+/**
+ * \class UserPlugin::IUserViewerWidget
+ * Acts like a QDataWidgetMapper over the Core::IUser model.
+ */
+
+/**
+ * \fn UserPlugin::IUserViewerWidget::IUserViewerWidget(QWidget *parent = 0)
+ * Ctor of the widget
+ */
+
+/**
+ * \fn UserPlugin::IUserViewerWidget::~IUserViewerWidget()
+ * Dtor of the widget
+ */
+
+/**
+ * \fn virtual void UserPlugin::IUserViewerWidget::setUserModel(UserModel *model) = 0;
+ * Define the UserPlugin::UserModel to use.
+ */
+
+/**
+ * \fn virtual void UserPlugin::IUserViewerWidget::setUserIndex(const int row) = 0;
+ * Define the current index. Model is defined before of course.
+ */
+
+/**
+ * \fn virtual void UserPlugin::IUserViewerWidget::clear() = 0;
+ * Clear the UI content, cache content...
+ */
+
+/**
+ * \fn virtual bool UserPlugin::IUserViewerWidget::submit() = 0;
+ * Submit changes to the model and return the state of error (true = no error, false = error).
+ */
 
