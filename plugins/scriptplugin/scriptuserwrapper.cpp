74e0f72acdf200131ae9c20cc3f4520b88133062
eric.maeker <eric.maeker@gmail.com>
2012-12-30 23:13:38 +0100

    script user wrapper

---
+++ plugins/scriptplugin/scriptuserwrapper.cpp
@@ -0,0 +1,118 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#include "scriptuserwrapper.h"
+
+#include <coreplugin/icore.h>
+#include <coreplugin/iuser.h>
+
+using namespace Script;
+using namespace Internal;
+
+static inline Core::IUser *user() {return Core::ICore::instance()->user();}
+
+ScriptUserWrapper::ScriptUserWrapper(QObject *parent) :
+    QObject(parent)
+{
+}
+
+bool ScriptUserWrapper::isActive() const
+{
+    return (!user()->uuid().isEmpty());
+}
+QString ScriptUserWrapper::fullName() const
+{
+    return user()->value(Core::IUser::FullName).toString();
+}
+
+QString ScriptUserWrapper::birthName() const
+{
+    return user()->value(Core::IUser::Name).toString();
+}
+
+QString ScriptUserWrapper::secondName() const
+{
+    return user()->value(Core::IUser::SecondName).toString();
+}
+
+QString ScriptUserWrapper::firstName() const
+{
+    return user()->value(Core::IUser::Firstname).toString();
+}
+
+QString ScriptUserWrapper::street() const
+{
+    return user()->value(Core::IUser::Address).toString();
+}
+
+QString ScriptUserWrapper::city() const
+{
+    return user()->value(Core::IUser::City).toString();
+}
+
+QString ScriptUserWrapper::zipcode() const
+{
+    return user()->value(Core::IUser::Zipcode).toString();
+}
+
+QString ScriptUserWrapper::state() const
+{
+    return user()->value(Core::IUser::StateProvince).toString();
+}
+
+QString ScriptUserWrapper::country() const
+{
+    return user()->value(Core::IUser::Country).toString();
+}
+
+QDate ScriptUserWrapper::dateOfBirth() const
+{
+    return user()->value(Core::IUser::DateOfBirth).toDate();
+}
+
+bool ScriptUserWrapper::isMale() const
+{
+    return (user()->value(Core::IUser::GenderIndex).toInt()==0);
+}
+
+bool ScriptUserWrapper::isFemale() const
+{
+    return user()->value(Core::IUser::GenderIndex).toInt()==1;
+}
+
+QStringList ScriptUserWrapper::specialties() const
+{
+    return user()->value(Core::IUser::Specialities).toStringList();
+}
+
+QStringList ScriptUserWrapper::qualifications() const
+{
+    return user()->value(Core::IUser::Qualifications).toStringList();
+}
+
+QStringList ScriptUserWrapper::identifiants() const
+{
+    return user()->value(Core::IUser::ProfessionalIdentifiants).toStringList();
+}
---
+++ plugins/scriptplugin/scriptuserwrapper.h
@@ -0,0 +1,98 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#ifndef SCRIPTPATIENTWRAPPER_H
+#define SCRIPTPATIENTWRAPPER_H
+
+#include <QObject>
+#include <QDate>
+#include <QVariant>
+#include <QString>
+#include <QStringList>
+
+/**
+ * \file scriptuserwrapper.h
+ * \author Eric Maeker
+ * \version 0.8.2
+ * \date 30 Dec 2012
+*/
+
+namespace Script {
+namespace Internal {
+
+class ScriptUserWrapper : public QObject  //, public QScriptClass
+{
+    Q_OBJECT
+    Q_PROPERTY(bool     isActive    READ isActive())
+    Q_PROPERTY(QString  birthName   READ birthName())
+    Q_PROPERTY(QString  secondName  READ secondName())
+    Q_PROPERTY(QString  firstName   READ firstName())
+    Q_PROPERTY(QString  fullName    READ fullName())
+
+    Q_PROPERTY(QString  street     READ street())
+    Q_PROPERTY(QString  city       READ city())
+    Q_PROPERTY(QString  zipcode    READ zipcode())
+    Q_PROPERTY(QString  state      READ state())
+    Q_PROPERTY(QString  country    READ country())
+
+    Q_PROPERTY(QDate    dateOfBirth READ dateOfBirth())
+    Q_PROPERTY(bool     isMale      READ isMale())
+    Q_PROPERTY(bool     isFemale    READ isFemale())
+
+    Q_PROPERTY(QStringList specialties READ specialties())
+    Q_PROPERTY(QStringList qualifications READ qualifications())
+    Q_PROPERTY(QStringList identifiants READ identifiants())
+
+public:
+    ScriptUserWrapper(QObject *parent);
+
+public Q_SLOTS:
+    bool isActive() const;
+
+    QString fullName() const;
+    QString birthName() const;
+    QString secondName() const;
+    QString firstName() const;
+
+    QString street() const;
+    QString city() const;
+    QString zipcode() const;
+    QString state() const;
+    QString country() const;
+
+    QDate dateOfBirth() const;
+    bool isMale() const;
+    bool isFemale() const;
+
+    QStringList specialties() const;
+    QStringList qualifications() const;
+    QStringList identifiants() const;
+
+};
+
+}  // namespace Internal
+}  //  End namespace Script
+
+#endif // SCRIPTPATIENTWRAPPER_H
