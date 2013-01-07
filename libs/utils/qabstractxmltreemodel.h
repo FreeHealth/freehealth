14aa458ab1eeb488c55af3db679f124b611f22c2
eric.maeker <eric.maeker@gmail.com>
2013-01-07 17:36:01 +0100

    adding exporter

--- libs/utils/qabstractxmltreemodel.h
+++ libs/utils/qabstractxmltreemodel.h
@@ -1,15 +1,50 @@
-#ifndef QABSTRACTXMLTREEMODEL_H
-#define QABSTRACTXMLTREEMODEL_H
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main developers : Eric MAEKER, <eric.maeker@gmail.com>                *
+ *   Contributors :                                                        *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#ifndef UTILS_QABSTRACTXMLTREEMODEL_H
+#define UTILS_QABSTRACTXMLTREEMODEL_H
 
+#include <utils/global_exporter.h>
 #include <QAbstractItemModel>
+QT_BEGIN_NAMESPACE
 class QDomNode;
+QT_END_NAMESPACE
+
+/**
+ * \file qabstractxmltreemodel.h
+ * \author Eric MAEKER
+ * \version 0.7.6
+ * \date 19 Jun 2012
+*/
 
 namespace Utils {
 namespace Internal {
 class QAbstractXmlTreeModelPrivate;
 }
 
-class QAbstractXmlTreeModel : public QAbstractItemModel
+class UTILS_EXPORT QAbstractXmlTreeModel : public QAbstractItemModel
 {
     Q_OBJECT
 public:
@@ -35,7 +70,6 @@ private:
     Internal::QAbstractXmlTreeModelPrivate *dqaxtm;
 };
 
-
 }  // End namespace Utils
 
-#endif // QABSTRACTXMLTREEMODEL_H
+#endif // UTILS_QABSTRACTXMLTREEMODEL_H
