2a9715517f3c3f5449a92697ff110ee42b740194
eric.maeker <eric.maeker@gmail.com>
2013-02-09 14:11:17 +0100

    avoid segfaulting if title id is wrong (outside titles list)

--- plugins/patientbaseplugin/patientmodel.cpp
+++ plugins/patientbaseplugin/patientmodel.cpp
@@ -977,6 +977,8 @@ bool PatientModel::refreshModel()
 QHash<QString, QString> PatientModel::patientName(const QList<QString> &uuids)
 {
     QHash<QString, QString> names;
+    if (uuids.isEmpty())
+        return names;
     QSqlDatabase DB = patientBase()->database();
     DB.transaction();
     QSqlQuery query(DB);
@@ -987,13 +989,25 @@ QHash<QString, QString> PatientModel::patientName(const QList<QString> &uuids)
             continue;
         QHash<int, QString> where;
         where.insert(Constants::IDENTITY_UID, QString("='%1'").arg(u));
-        QString req = patientBase()->select(Constants::Table_IDENT, QList<int>() << Constants::IDENTITY_TITLE << Constants::IDENTITY_BIRTHNAME << Constants::IDENTITY_SECONDNAME << Constants::IDENTITY_FIRSTNAME, where);
+        QString req = patientBase()->select(Constants::Table_IDENT,
+                                            QList<int>()
+                                            << Constants::IDENTITY_TITLE
+                                            << Constants::IDENTITY_BIRTHNAME
+                                            << Constants::IDENTITY_SECONDNAME
+                                            << Constants::IDENTITY_FIRSTNAME,
+                                            where);
         if (query.exec(req)) {
             if (query.next()) {
-                if (!query.value(1).toString().isEmpty())
-                    names.insert(u, QString("%1 %2 - %3 %4").arg(titles.at(query.value(0).toInt()), query.value(1).toString(), query.value(2).toString(), query.value(3).toString()).simplified());
-                else
-                    names.insert(u, QString("%1 %3").arg(titles.at(query.value(0).toInt()), query.value(2).toString(), query.value(3).toString()).simplified());
+                QString title;
+                int titleId = query.value(0).toInt();
+                if (IN_RANGE_STRICT_MAX(titleId, 0, titles.count()))
+                    title = titles.at(titleId);
+                const QString &secondName = query.value(2).toString();
+                if (!secondName.isEmpty()) {
+                    names.insert(u, QString("%1 %2 - %3 %4").arg(title, query.value(1).toString(), secondName, query.value(3).toString()).simplified());
+                } else {
+                    names.insert(u, QString("%1 %2 %3").arg(title, query.value(2).toString(), query.value(3).toString()).simplified());
+                }
             }
         } else {
             LOG_QUERY_ERROR_FOR("PatientModel", query);
