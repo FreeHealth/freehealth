db26e51ec5138d92c633fb160bfedbfe85cc0305
eric.maeker <eric.maeker@gmail.com>
2013-02-11 18:08:49 +0100

    by default add an help icon in lineedits (default is no database)

--- plugins/zipcodesplugin/zipcodescompleters.cpp
+++ plugins/zipcodesplugin/zipcodescompleters.cpp
@@ -401,7 +401,7 @@ void ZipCountryCompleters::setCityLineEdit(Utils::QButtonLineEdit *city)
 
     // button
     m_CityButton = new QToolButton(m_cityEdit);
-    m_CityButton->setIcon(theme()->icon(Core::Constants::ICONOK));
+    m_CityButton->setIcon(theme()->icon(Core::Constants::ICONHELP));
     m_cityEdit->setRightButton(m_CityButton);
 }
 
@@ -422,7 +422,7 @@ void ZipCountryCompleters::setZipLineEdit(Utils::QButtonLineEdit *zip)
 
     // button
     m_ZipButton = new QToolButton(m_zipEdit);
-    m_ZipButton->setIcon(theme()->icon(Core::Constants::ICONOK));
+    m_ZipButton->setIcon(theme()->icon(Core::Constants::ICONHELP));
     m_zipEdit->setRightButton(m_ZipButton);
 }
 
