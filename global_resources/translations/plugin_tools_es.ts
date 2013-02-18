652ccb8d32697df3ec0b1480bc4f188d9f4407e2
eric.maeker <eric.maeker@gmail.com>
2013-02-18 23:03:57 +0100

    new trans

---
+++ global_resources/translations/plugin_tools_es.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>Tools::Internal::PdfTkWrapper</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="329"/>
+        <source>Starting PDF completion. Please wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="349"/>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="356"/>
+        <source>PDF Completion error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="350"/>
+        <source>The PDF completion process raised the following error:&lt;br /&gt;&lt;b&gt;%1&lt;/b&gt;
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="379"/>
+        <source>pdftk error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="380"/>
+        <source>The pdftk process ends with the exit code: %1
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="390"/>
+        <source>Opening completed PDF file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPlugin</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolsplugin.cpp" line="135"/>
+        <source>Initializing Tools...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPreferencesPage</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.cpp" line="151"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.cpp" line="164"/>
+        <source>TOOLS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPreferencesWidget</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.ui" line="14"/>
+        <source>Tools Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ global_resources/translations/plugin_tools_fr.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Tools::Internal::PdfTkWrapper</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="329"/>
+        <source>Starting PDF completion. Please wait...</source>
+        <translation>Initialise la complétion du formulaire PDF. Veuillez patienter...</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="349"/>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="356"/>
+        <source>PDF Completion error</source>
+        <translation>Erreur lors de la complétion du formulaire PDF</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="350"/>
+        <source>The PDF completion process raised the following error:&lt;br /&gt;&lt;b&gt;%1&lt;/b&gt;
+%2</source>
+        <translation>La complétion du formulaire PDF a levé l&apos;erreur suivante : &lt;br /&gt;&lt;b&gt;%1&lt;/b&gt;
+%2</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="379"/>
+        <source>pdftk error</source>
+        <translation>Erreur complétion PDF</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="380"/>
+        <source>The pdftk process ends with the exit code: %1
+%2</source>
+        <translation>Le processus de complétion des formulaires PDF s&apos;est terminé avec le code d&apos;erreur : %1
+%2</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/pdftkwrapper.cpp" line="390"/>
+        <source>Opening completed PDF file</source>
+        <translation>Ouverture du formulaire PDF complété</translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPlugin</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolsplugin.cpp" line="135"/>
+        <source>Initializing Tools...</source>
+        <translation>Initialisation des outils...</translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPreferencesPage</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.cpp" line="151"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.cpp" line="164"/>
+        <source>TOOLS</source>
+        <translation>Outils</translation>
+    </message>
+</context>
+<context>
+    <name>Tools::Internal::ToolsPreferencesWidget</name>
+    <message>
+        <location filename="../../plugins/toolsplugin/toolspreferences.ui" line="14"/>
+        <source>Tools Preferences</source>
+        <translation>Préférences des outils</translation>
+    </message>
+</context>
+</TS>
