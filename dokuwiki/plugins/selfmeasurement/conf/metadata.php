7be197a1439721485a4a00aa000bf16bc0fa1045
eric.maeker <eric.maeker@gmail.com>
2013-03-04 09:24:37 +0100

    new dokuwiki plugin (blood pressure self-measurements)

---
+++ dokuwiki/plugins/selfmeasurement/conf/metadata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * DokuWiki Plugin selfmeasurement (Syntax Component)
+ *
+ * @license BSD-3 Clause http://www.gnu.org/licenses/bsd.html
+ * @author  Eric Maeker, MD (fr) <eric.maeker@gmail.com>
+ */
+
+$meta['automeasurement_sendto']      = array('email');
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+
