d1e98a4829f004ad35ab003eee28714cdb527f62
eric.maeker <eric.maeker@gmail.com>
2013-03-17 23:07:42 +0100

    compil complaining

--- plugins/account2plugin/models/feemodel.h
+++ plugins/account2plugin/models/feemodel.h
@@ -63,7 +63,7 @@ public:
     ~FeeModel();
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const {return ColumnCount;}
+    int columnCount(const QModelIndex & = QModelIndex()) const {return ColumnCount;}
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
