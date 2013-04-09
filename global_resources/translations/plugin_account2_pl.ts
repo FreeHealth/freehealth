9040e89db001fa808550b883df1865e254c32d0f
eric.maeker <eric.maeker@gmail.com>
2013-04-09 17:21:58 +0200

    translations

---
+++ global_resources/translations/plugin_account2_pl.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>Account2::Internal::Account2Plugin</name>
+    <message>
+        <location filename="../../plugins/account2plugin/account2plugin.cpp" line="97"/>
+        <source>Initializing accountancy plugin...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Account2::Internal::AccountMode</name>
+    <message>
+        <location filename="../../plugins/account2plugin/accountmode.cpp" line="78"/>
+        <source>Account2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
