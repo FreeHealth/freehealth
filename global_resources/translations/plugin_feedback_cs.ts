fe4dacac72655b6c09c98fef3b5ed36c661bcea0
eric.maeker <eric.maeker@gmail.com>
2013-05-01 16:35:24 +0200

    translations

---
+++ global_resources/translations/plugin_feedback_cs.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>Feedback::BugReportDialog</name>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="156"/>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="229"/>
+        <source>&amp;Send report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="157"/>
+        <source>Problem reporting dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="185"/>
+        <source>The email address you entered is not valid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="190"/>
+        <source>Please choose the issue category.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="194"/>
+        <source>Please select the severity of the issue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="198"/>
+        <source>Describe the problem you have in short, pregnant words.&lt;br/&gt;The more information you provide, the easier we can help.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="202"/>
+        <source>Please confirm that you have looked up the documentation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.cpp" line="205"/>
+        <source>&lt;p&gt;The following problems occurred:&lt;/p&gt;&lt;ul&gt;%1&lt;/ul&gt;</source>
+        <extracomment>tooltip for bug report &quot;send&quot; button</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Feedback::Internal::BugReportDialog</name>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="14"/>
+        <source>Issue reporting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="23"/>
+        <source>First step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="29"/>
+        <source>Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="39"/>
+        <source>Application:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="49"/>
+        <source>yourname@gmail.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="63"/>
+        <source>OS:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="73"/>
+        <source>&amp;Email:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="86"/>
+        <source>Current problem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="92"/>
+        <source>&amp;Category:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="105"/>
+        <source>&amp;Severity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="118"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="153"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&amp;amp;Yes, I have checked the documentation on the &lt;a href=&quot;http://www.freemedforms.com/en/manuals/freemedforms/toc&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;FreeMedForms Project website&lt;/span&gt;&lt;/a&gt; before reporting this issue.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/bugreportdialog.ui" line="168"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When you click on &lt;span style=&quot; font-style:italic;&quot;&gt;Send&lt;/span&gt;, debug information is collected and sent along with the report.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Feedback::Internal::FeedbackPlugin</name>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="146"/>
+        <source>Initializing Feedback...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="182"/>
+        <source>General comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="183"/>
+        <source>Forms management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="184"/>
+        <source>Patient management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="185"/>
+        <source>User management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="186"/>
+        <source>Drugs management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="187"/>
+        <source>Agenda management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="188"/>
+        <source>Printings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="189"/>
+        <source>Installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/feedbackplugin/feedbackplugin.cpp" line="190"/>
+        <source>Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
