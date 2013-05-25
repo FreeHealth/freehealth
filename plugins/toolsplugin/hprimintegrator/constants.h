2fb72c9dfc2dcd49aa677522e06dec1a13d110c1
eric.maeker <eric.maeker@gmail.com>
2013-05-25 12:27:13 +0200

    header & constants

--- plugins/toolsplugin/hprimintegrator/constants.h
+++ plugins/toolsplugin/hprimintegrator/constants.h
@@ -1,10 +1,39 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *   Main Developpers:                                                     *
+ *       Eric Maeker <e>                             *
+ *   Contributors:                                                         *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
 #ifndef HPRIM_INTEGRATOR_CONSTANTS_H
 #define HPRIM_INTEGRATOR_CONSTANTS_H
 
 namespace Tools {
 namespace Constants {
 
-const char *const S_DEFAULT_FILE_ENCODING = "Tools/HprimIntegrator/DefaultFileEncoding";
+const char *const S_DEFAULT_FILE_ENCODING   = "Tools/HprimIntegrator/DefaultFileEncoding";
+const char *const S_PATH_TO_SCAN            = "Tools/HprimIntegrator/PathToScan";
+const char *const S_FORMITEM_UUIDS          = "Tools/HprimIntegrator/FormItemUuids";
+const char *const S_FILE_MANAGEMENT         = "Tools/HprimIntegrator/FileManagement";
 
 } // namespace Constants
 } // namespace Tools
