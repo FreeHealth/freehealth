d471060e21e073d3e1ab9487e0505fba53d9a570
eric.maeker <eric.maeker@gmail.com>
2013-09-19 18:08:26 +0200

    win script: change unix line feed to win32

--- scripts/win/win_release.bat
+++ scripts/win/win_release.bat
@@ -1,5 +1,5 @@
 REM # This script is part of FreeMedForms project : http://www.freemedforms.com
-REM # (C) 2008-2012 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>
+REM # (C) 2008-2013 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>
 REM # License: BSD 3-clause
 REM #
 REM # This script compiles and create the installer for any FreeMedForms application
@@ -9,6 +9,7 @@ REM # - the freemedforms-project source package is decompressed here
 REM # - the most recent MySQL is installed (see %PATH_TO_MYSQL%)
 REM # - the most recent OpenCV must be installed in the contrib path 
 REM # - the Inno Setup 5 is installed on the machine (see %PATH_TO_INNOSETUP%)
+REM # - the unix2dos.exe is installed on the machine in the %PATH%
 REM # 
 REM # After the compilation, you will find the installer in the source root dir
 
@@ -30,7 +31,7 @@ REM # Go to application source tree
 cd %1
 
 REM # Compil application && install it
-qmake.exe %1.pro -r -spec win32-g++ CONFIG+=release CONFIG-=debug_and_release CONFIG+=with-alerts CONFIG+=with-pad CONFIG+=with-agenda CONFIG+=with-pmh
+qmake.exe %1.pro -r -spec win32-g++ CONFIG+=release CONFIG-=debug_and_release
 mingw32-make.exe -w
 mingw32-make.exe install
 
@@ -42,6 +43,10 @@ REM # Copy MinGW lib into the package dir
 copy %PATH_TO_MINGW%\libgcc_s_dw2-1.dll ..\packages\win\%1\
 copy %PATH_TO_MINGW%\libstdc*-6.dll ..\packages\win\%1\
 copy %PATH_TO_MINGW%\mingwm10.dll ..\packages\win\%1\
+
+REM # Change linefeed on TXT files
+unix2dos ..\packages\win\%1\README.txt
+unix2dos ..\packages\win\%1\COPYING.txt
 
 REM # Create the installer
 %PATH_TO_INNOSETUP% "%WORKING_DIRECTORY%/../../global_resources/package_helpers/%1.iss"
