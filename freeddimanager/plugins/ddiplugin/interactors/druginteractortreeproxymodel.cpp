33075af57e34edb6420f675dd341a8fc00f1eb14
eric.maeker <eric.maeker@gmail.com>
2013-10-12 18:35:04 +0200

    preparing drug interactor tree model

---
+++ freeddimanager/plugins/ddiplugin/interactors/druginteractortreeproxymodel.cpp
@@ -0,0 +1,187 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2013 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *  Main Developer: Eric MAEKER, <eric.maeker@gmail.com>                   *
+ *  Contributors:                                                          *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#include "druginteractortreeproxymodel.h"
+#include "druginteractortablemodel.h"
+
+#include <coreplugin/icore.h>
+#include <coreplugin/imainwindow.h>
+#include <coreplugin/isettings.h>
+
+#include <utils/log.h>
+#include <utils/global.h>
+#include <translationutils/constanttranslations.h>
+
+#include <QString>
+#include <QStandardItem>
+#include <QPointer>
+
+#include <QDebug>
+
+using namespace DDI;
+using namespace Internal;
+using namespace Trans::ConstantTranslations;
+
+static inline Core::IMainWindow *mainwindow() {return Core::ICore::instance()->mainWindow();}
+static inline Core::ISettings *settings()  { return Core::ICore::instance()->settings(); }
+
+namespace {
+const int ENGLISH = Qt::UserRole + 1;
+const int FRENCH = ENGLISH + 1;
+const int SPANISH = FRENCH + 1;
+const int CODE = SPANISH + 1;
+}  // anymous namespace
+
+namespace DDI {
+namespace Internal {
+class DrugInteractorTreeProxyModelPrivate
+{
+public:
+    DrugInteractorTreeProxyModelPrivate(DrugInteractorTreeProxyModel *parent) :
+        _sourceModel(0),
+        q(parent)
+    {
+        bold.setBold(true);
+    }
+
+    ~DrugInteractorTreeProxyModelPrivate()
+    {}
+
+    QModelIndex toSourceIndex(const QModelIndex &index)
+    {
+        // Map to Code column
+        QModelIndex code = q->index(index.row(), DrugInteractorTreeProxyModel::Code, index.parent());
+        return _toSourceIndex.value(q->itemFromIndex(code), QModelIndex());
+    }
+
+    bool getTree(DrugInteractorTableModel *sourceModel)
+    {
+        _toSourceIndex.clear();
+        // TODO: disconnect old model if exists
+        _sourceModel = sourceModel;
+
+        // Fetch all data in the source model
+        while (_sourceModel->canFetchMore(_sourceModel->index(0, _sourceModel->rowCount())))
+            _sourceModel->fetchMore(_sourceModel->index(0, _sourceModel->rowCount()));
+
+        // Sort ?
+
+
+//        // Read line by line
+//        QStandardItem *lastOne = 0;
+//        QStandardItem *lastThree = 0;
+//        QStandardItem *lastFour = 0;
+//        QStandardItem *lastFive = 0;
+//        QStandardItem *parent = 0;
+//        QList<QStandardItem*> cols;
+//        for(int i=0; i < _sourceModel->rowCount(); ++i) {
+//            cols.clear();
+//            QModelIndex code = _sourceModel->index(i, DrugInteractorTableModel::Code);
+//            QModelIndex fr = _sourceModel->index(i, DrugInteractorTableModel::LabelFr);
+//            QModelIndex en = _sourceModel->index(i, DrugInteractorTableModel::LabelEn);
+//            QModelIndex de = _sourceModel->index(i, DrugInteractorTableModel::LabelDe);
+
+//            QStandardItem *item = new QStandardItem(_sourceModel->data(code).toString());
+//            _toSourceIndex.insert(item, code);
+//            cols << item;
+//            cols <<  new QStandardItem(_sourceModel->data(en).toString());
+//            cols <<  new QStandardItem(_sourceModel->data(fr).toString());
+//            cols <<  new QStandardItem(_sourceModel->data(de).toString());
+//            if (_sourceModel->data(code).toString().size() == 1) {
+//                foreach(QStandardItem *item, cols)
+//                    item->setFont(bold);
+//            }
+
+//            // Find the parent item
+//            switch (_sourceModel->data(code).toString().count()) {
+//            case 1: parent = 0; lastOne = item; break;
+//            case 3: parent = lastOne;    lastThree = item; break;
+//            case 4: parent = lastThree;  lastFour = item;  break;
+//            case 5: parent = lastFour;   lastFive = item;  break;
+//            case 7: parent = lastFive;    break;
+//            }
+//            // need to be reparented
+//            if (!parent)
+//                parent = q->invisibleRootItem();
+//            parent->appendRow(cols);
+//            parent = 0;
+//        }
+        return true;
+    }
+
+public:
+    QFont bold;
+    QPointer<DrugInteractorTableModel> _sourceModel;
+    QHash<QStandardItem *, QPersistentModelIndex> _toSourceIndex;
+
+private:
+    DrugInteractorTreeProxyModel *q;
+};
+
+}  // namespace Internal
+}  // namespace DDI
+
+DrugInteractorTreeProxyModel::DrugInteractorTreeProxyModel(QObject * parent) :
+    QStandardItemModel(parent),
+    d(new DrugInteractorTreeProxyModelPrivate(this))
+{
+}
+
+DrugInteractorTreeProxyModel::~DrugInteractorTreeProxyModel()
+{}
+
+/** Initialize ATC Model. Read the ATC CSV file in the global_resources */
+bool DrugInteractorTreeProxyModel::initialize(DrugInteractorTableModel *sourceModel)
+{
+    d->getTree(sourceModel);
+    return true;
+}
+
+Qt::ItemFlags DrugInteractorTreeProxyModel::flags(const QModelIndex &index) const
+{
+    Q_UNUSED(index);
+    return Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsEditable;
+}
+
+QVariant DrugInteractorTreeProxyModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
+        switch (section) {
+        case Code: return tr("Code");
+        // case Uid: return tr("Uid");
+        case LabelEn: return tr("English");
+        case LabelFr: return tr("French");
+        case LabelDe: return tr("Deustch");
+        // case LabelSp: return tr("Spanish");
+        }
+    }
+    return QVariant();
+}
+
+QModelIndex DrugInteractorTreeProxyModel::toSourceIndex(const QModelIndex &index) const
+{
+    return d->toSourceIndex(index);
+}
---
+++ freeddimanager/plugins/ddiplugin/interactors/druginteractortreeproxymodel.h
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *  The FreeMedForms project is a set of free, open source medical         *
+ *  applications.                                                          *
+ *  (C) 2008-2013 by Eric MAEKER, MD (France) <eric.maeker@gmail.com>      *
+ *  All rights reserved.                                                   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program (COPYING.FREEMEDFORMS file).                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+/***************************************************************************
+ *  Main Developer: Eric MAEKER, MD <eric.maeker@gmail.com>                *
+ *  Contributors:                                                          *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ *       NAME <MAIL@ADDRESS.COM>                                           *
+ ***************************************************************************/
+#ifndef DDIMANAGER_DDIPLUGIN_INTERACTORTREEPROXYMODEL_H
+#define DDIMANAGER_DDIPLUGIN_INTERACTORTREEPROXYMODEL_H
+
+#include <ddiplugin/ddi_exporter.h>
+#include <QStandardItemModel>
+
+/**
+ * \file druginteractortreeproxymodel.h
+ * \author Eric MAEKER
+ * \version 0.10.0
+ * \date 12 Oct 2013
+*/
+
+namespace DDI {
+class DrugInteractorTableModel;
+namespace Internal {
+class DrugInteractorTreeProxyModelPrivate;
+}
+
+class DDI_EXPORT DrugInteractorTreeProxyModel : public QStandardItemModel
+{
+    Q_OBJECT
+
+public:
+    enum DataRepresentation {
+        Code = 0,
+        LabelEn,
+        LabelFr,
+        LabelDe,
+        NumberOfColumn
+    };
+    DrugInteractorTreeProxyModel(QObject * parent = 0);
+    bool initialize(DrugInteractorTableModel *sourceModel);
+
+    ~DrugInteractorTreeProxyModel();
+
+//    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const;
+
+    QModelIndex toSourceIndex(const QModelIndex &index) const;
+
+private:
+    Internal::DrugInteractorTreeProxyModelPrivate *d;
+};
+
+}  //  End namespace DDI
+
+#endif // DDIMANAGER_DDIPLUGIN_INTERACTORTREEPROXYMODEL_H
