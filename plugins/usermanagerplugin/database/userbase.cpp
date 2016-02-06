15a19fffc7750e5990e7aab5fc6fabf8bb99c178
Jerome Pinguet <jerome@jerome.cc>
2016-02-06 17:41:22 +0100

    completes issue #105 adding default to switch
    to avoid warning "control reaches end of non-void function [-Wreturn-type]"

--- plugins/usermanagerplugin/database/userbase.cpp
+++ plugins/usermanagerplugin/database/userbase.cpp
@@ -632,6 +632,7 @@ bool UserBase::userExists(const QString &clearLogin) const
         {                                                                           
             return false;                                                           
         }
+        default: return false;
     }
 }
 
