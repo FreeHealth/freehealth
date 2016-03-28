48f61ed5f2aa7540ccbdcfe1ab62fa2f8c8aa6b7
Jerome Pinguet <jerome@jerome.cc>
2016-03-28 19:11:02 +0200

    issue #51
    compute BMI in SOAP form

--- global_resources/forms/subforms/generic/soap_with_biomeasurements/scripts/xx/script.js
+++ global_resources/forms/subforms/generic/soap_with_biomeasurements/scripts/xx/script.js
@@ -2,11 +2,13 @@ namespace.module('com.freemedforms.generic.soapWithBio', function (exports, requ
 
     exports.extend({
                        'setupUi': setupUi,
-                       'computePulsePressure': computePulsePressure
+                       'computePulsePressure': computePulsePressure,
+                       'computeBMI' : computeBMI
                    });
 
     // Ui vars (retrieved from the ui)
     var syst, diast, pulse;
+    var weight, weightUnit, weightUnitItem, height, heightUnit, heightUnitItem, bmiValueLabel, bmiValueLineEdit;
 
     function setupUi() {
         print("com.freemedforms.generic.soapWithBio Setup UI");
@@ -19,6 +21,14 @@ namespace.module('com.freemedforms.generic.soapWithBio', function (exports, requ
         syst = formUi.findChild("bpSyst");
         diast = formUi.findChild("bpDiast");
         pulse = formUi.findChild("bpPulse");
+        bmiValueLabel = formUi.findChild("bmiValueLabel");
+        bmiValueLineEdit = formUi.findChild("bmiValueLineEdit");
+        weight = formUi.findChild("weight");
+        weightUnit = formUi.findChild("weightUnit");
+        weightUnitItem = freemedforms.forms.item("Subs::Tools::SOAP::WithBio::ObjectiveGroup::Weight::Value");
+        height = formUi.findChild("height");
+        heightUnit = formUi.findChild("heightUnit");
+        heightUnitItem = freemedforms.forms.item("Subs::Tools::SOAP::WithBio::ObjectiveGroup::Height::Value");
 
 //      populateCombos();
         
@@ -28,6 +38,14 @@ namespace.module('com.freemedforms.generic.soapWithBio', function (exports, requ
 
         syst['valueChanged(int)'].connect(this, computePulsePressure);          
         diast['valueChanged(int)'].connect(this, computePulsePressure);
+
+        weight['valueChanged(double)'].connect(this, computeBMI);
+        height['valueChanged(double)'].connect(this, computeBMI);
+        weightUnit['currentIndexChanged(int)'].connect(this, computeBMI);
+        heightUnit['currentIndexChanged(int)'].connect(this, computeBMI);
+
+        // hide bmiValueLineEdit
+        bmiValueLineEdit.visible = false;
     }
 
 //     function populateCombos() {                                                 
@@ -44,6 +62,77 @@ namespace.module('com.freemedforms.generic.soapWithBio', function (exports, requ
     function computePulsePressure() {
         var text = syst.value - diast.value;                                    
         pulse.setText(text);                                                    
+    }
+
+    function roundToOne(num) {
+        return +(Math.round(num + "e+1")  + "e-1");
+    }
+
+    function weightToKilogram(weightUnit, weightValue) {
+        print(weightUnit,weightValue);
+        var ounceToKilogram = 0.028349523125; //International avoirdupois ounce
+        var poundToKilogram = 0.45359237; //International avoirdupois pound
+        var gramToKilogram = 0.001;
+        var kilogram = "Kilogram";
+        var gram = "Gram";
+        var ounce = "Ounce";
+        var pound = "Pound";
+        if (~weightUnit.indexOf(gram)) {
+            print("gram to kg", weightValue*gramToKilogram);
+            return weightValue*gramToKilogram;
+        } else if (~weightUnit.indexOf(kilogram)) {
+            return weightValue;
+        } else if (~weightUnit.indexOf(ounce)) {
+            return weightValue*ounceToKilogram;
+        } else if (~weightUnit.indexOf(pound)) {
+            return weightValue*poundToKilogram;
+        }
+    }
+
+    function heightToMeter(heightUnit, heightValue) {
+        var centimeterToMeter = 0.01;
+        var inchToMeter = 0.0254;
+        var footToMeter = 0.3048;
+        var centimeter = "Centimeter";
+        var meter = "Meter";
+        var inch = "Inch";
+        var foot = "Foot";
+        if (~heightUnit.indexOf(centimeter)) {
+            return heightValue*centimeterToMeter;
+        } else if (~heightUnit.indexOf(meter)) {
+            return heightValue;
+        } else if (~heightUnit.indexOf(inch)) {
+            return heightValue*inchToMeter;
+        } else if (~heightUnit.indexOf(foot)) {
+            return heightValue*footToMeter;
+        }
+    }
+
+    function computeBMI() {
+        //Number.prototype.round = function(places) {
+        //return +(Math.round(this + "e+" + places)  + "e-" + places);
+        //}
+        var bmi = Number(textbmi);
+        // weight unit kilogram
+        // height unit meter
+        var weightUnit = weightUnitItem.currentText;
+        var weightValue = Number(weight.value);
+        weightKilogram = weightToKilogram(weightUnit, weightValue);
+
+        var heightUnit = heightUnitItem.currentText;
+        var heightValue = Number(height.value);
+        heightMeter = heightToMeter(heightUnit, heightValue);
+
+        var bmi  = (weightKilogram) / ((heightMeter) * (heightMeter));
+        bmi = roundToOne(bmi);
+        var textbmi = bmi.toString();
+        if (!isNaN(bmi)) {
+        bmiValueLineEdit.setText(textbmi);
+        bmiValueLabel.setText(textbmi);
+        } else {
+        bmiValueLineEdit.setText("");
+        bmiValueLabel.setText("");
+        }
     }
 });
 
